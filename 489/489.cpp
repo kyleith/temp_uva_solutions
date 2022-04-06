1059db31894be96d01811c13582a00a582a19504
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-07 00:47:48 +0700

    solution #489

--- 489/489.cpp
+++ 489/489.cpp
@@ -8,7 +8,6 @@
 #define u_int unsigned int
 #define t_wordLetters std::set <char>
 
-const u_int MAX_ROUNDS = 1000000;
 const u_int MAX_STROKES = 7;
 
 enum t_roundResult
@@ -19,18 +18,13 @@ enum t_roundResult
     RESULT_GAME_NOT_FINISHED
 };
 
-//ROUNDS...
-void initializeResultsArray (t_roundResult * results);
-void printResults (const t_roundResult * results, const u_int & roundsCount);
-//...ROUNDS
-
 //SINGLE ROUND...
-t_wordLetters transformWordIntoSet (const std::string & word);
-t_roundResult playRound (const std::string & guessWord, t_wordLetters & secretLetters);
+t_roundResult playRound (const std::string & secretWord, const std::string & guessLetters);
+void printResult (const t_roundResult & result, const u_int & roundNumber);
 //...SINGLE ROUND
 
 //HELPERS...
-bool isSupportedWordLetter (const char & symbol);
+t_wordLetters transformWordIntoSet (const std::string & word);
 bool isSymbolFoundInSet (const t_wordLetters & charSet, const char & symbol);
 //...HELPERS
 
@@ -41,123 +35,87 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
-    t_roundResult * results = new t_roundResult [MAX_ROUNDS];
-    initializeResultsArray(results);
-
-    u_int totalRoundsCount = 0;
-
     int currentRound = -2;
     while (std::cin >> currentRound, currentRound != -1)
     {
         std::string secretWord, guessWord;
         std::cin >> secretWord >> guessWord;
-       
-        if (totalRoundsCount < currentRound)
-        {
-            totalRoundsCount = currentRound;
-        }
 
-        t_wordLetters secretWordLetters = transformWordIntoSet(secretWord);
-        t_roundResult currentRoundResult = playRound(guessWord, secretWordLetters);
+        t_roundResult currentRoundResult = playRound(secretWord, guessWord);
         
-        results[currentRound - 1] = currentRoundResult;
+        printResult(currentRoundResult, currentRound);
     }
-
-    printResults(results, totalRoundsCount);
 
     return 0;
 }
 
-//ROUNDS...
-void initializeResultsArray (t_roundResult * results)
-{
-    for (u_int i = 0; i < MAX_ROUNDS; i++)
-    {
-        results[i] = RESULT_INVALID;
-    }
-}
-
-void printResults (const t_roundResult * results, const u_int & roundsCount)
-{
-    for (u_int i = 0; i < roundsCount; i++)
-    {
-        printf("Round %u\n", i + 1);
-
-        switch (results[i])
-        {
-            case RESULT_PLAYER_WIN:
-                printf("You win.\n");
-                break;
-            case RESULT_PLAYER_LOSE:
-                printf("You lose.\n");
-                break;
-            case RESULT_GAME_NOT_FINISHED:
-                printf("You chickened out.\n");
-                break;
-            default:
-                printf("Error.\n");
-                break;
-        }
-    }
-}
-//...ROUNDS
-
 //SINGLE ROUND...
-t_wordLetters transformWordIntoSet (const std::string & word)
+t_roundResult playRound (const std::string & secretWord, const std::string & guessLetters)
 {
-    t_wordLetters letters;
-    for (u_int i = 0; i < word.length(); i++)
-    {
-        char currentSymbol = word[i];
-        if (isSupportedWordLetter(currentSymbol))
-        {
-            letters.insert(currentSymbol);
-        }
-    }
-    return letters;
-}
+    t_wordLetters secretLetters = transformWordIntoSet(secretWord);
 
-t_roundResult playRound (const std::string & guessLetters, t_wordLetters & secretLetters)
-{
     u_int strokes = 0;
     t_wordLetters usedLetters;
     for (u_int i = 0; i < guessLetters.length(); i++)
     {
         char currentSymbol = guessLetters[i];
 
-        if (isSupportedWordLetter(currentSymbol))
+        bool secretSymbolFound = isSymbolFoundInSet(secretLetters, currentSymbol);
+        if (secretSymbolFound)
         {
-            bool secretSymbolFound = isSymbolFoundInSet(secretLetters, currentSymbol);
-            if (secretSymbolFound)
+            secretLetters.erase(currentSymbol);
+            if (secretLetters.empty())
             {
-                secretLetters.erase(currentSymbol);
-                if (secretLetters.empty())
-                {
-                    return RESULT_PLAYER_WIN;
-                }
+                return RESULT_PLAYER_WIN;
             }
-            else if (!isSymbolFoundInSet(usedLetters, currentSymbol))
-            {
-                strokes++;
-                if (strokes >= MAX_STROKES)
-                {
-                    return RESULT_PLAYER_LOSE;
-                }
-            }
-
-            usedLetters.insert(currentSymbol);
         }
+        else if (!isSymbolFoundInSet(usedLetters, currentSymbol))
+        {
+            strokes++;
+            if (strokes >= MAX_STROKES)
+            {
+                return RESULT_PLAYER_LOSE;
+            }
+        }
+
+        usedLetters.insert(currentSymbol);
     }
 
     return RESULT_GAME_NOT_FINISHED;
 }
+
+void printResult (const t_roundResult & result, const u_int & roundNumber)
+{
+    printf("Round %u\n", roundNumber);
+
+    switch (result)
+    {
+        case RESULT_PLAYER_WIN:
+            printf("You win.\n");
+            break;
+        case RESULT_PLAYER_LOSE:
+            printf("You lose.\n");
+            break;
+        case RESULT_GAME_NOT_FINISHED:
+            printf("You chickened out.\n");
+            break;
+        default:
+            printf("Error.\n");
+            break;
+    }
+}
 //...SINGLE ROUND
 
 //HELPERS...
-bool isSupportedWordLetter (const char & symbol)
+t_wordLetters transformWordIntoSet (const std::string & word)
 {
-    return (symbol >= 'a')
-        && (symbol <= 'z');
+    t_wordLetters letters;
+    for (u_int i = 0; i < word.length(); i++)
+    {
+        char currentSymbol = word[i];
+        letters.insert(currentSymbol);
+    }
+    return letters;
 }
 
 bool isSymbolFoundInSet (const t_wordLetters & charSet, const char & symbol)
