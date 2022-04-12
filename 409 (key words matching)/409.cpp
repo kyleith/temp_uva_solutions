a9b88ba2dd4257b113b72c04cefbbd82f4c92763
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-12 22:42:13 +0700

    #409 in progress

--- 409 (key words matching)/409.cpp
+++ 409 (key words matching)/409.cpp
@@ -21,8 +21,8 @@ void readAndProcessExcuse ( const char keywords[g_MAX_KEYWORDS_COUNT][g_MAX_KEYW
     );
 
 
-u_int findWorstExcuseCoeff ();
-void printExcuse ();
+u_int findWorstExcuseCoeff (const u_int coeffs [g_MAX_EXCUSES_COUNT], const int & count);
+void printExcuse (const char excuse [g_MAX_EXCUSE_LENGTH + 1]);
 
 void omitLineEnding ();
 
@@ -57,7 +57,16 @@ void processTestCase (const u_int & testCaseIndex, const u_int & keywordsCount, 
         readAndProcessExcuse(keywordsDictionary, keywordsCount, excusesList[i], excusesCoeffs[i]);
     }
 
-    //TODO...
+    printf("Excuse set #%u\n", testCaseIndex);
+    u_int worstCoeff = findWorstExcuseCoeff(excusesCoeffs, excusesCount);
+    for (u_int i = 0; i < excusesCount; i++)
+    {
+        if (excusesCoeffs[i] == worstCoeff)
+        {
+            printExcuse(excusesList[i]);
+        }
+    }
+    printf("\n");
 }
 
 void readWordsDictionary (const u_int & wordsCount, char dictionary[g_MAX_KEYWORDS_COUNT][g_MAX_KEYWORD_LENGTH + 1])
@@ -88,16 +97,26 @@ void readAndProcessExcuse ( const char keywords[g_MAX_KEYWORDS_COUNT][g_MAX_KEYW
         excuseLength++;
     }
     currentExcuse[excuseLength] = '\0';
+
+    //TODO: coeff calculation
 }
 
-u_int findWorstExcuseCoeff ()
+u_int findWorstExcuseCoeff (const u_int coeffs [g_MAX_EXCUSES_COUNT], const int & count)
 {
-    return 0;
+    u_int maxCoeff = 0;
+    for (u_int i = 0; i < count; i++)
+    {
+        if (coeffs[i] > maxCoeff)
+        {
+            maxCoeff = coeffs[i];
+        }
+    }
+    return maxCoeff;
 }
 
-void printExcuse ()
+void printExcuse (const char excuse [g_MAX_EXCUSE_LENGTH + 1])
 {
-
+    printf("%s\n", excuse);
 }
 
 void omitLineEnding ()
