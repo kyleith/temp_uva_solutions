56ce3b5918c2fcb12f993e2640131d5f1d5f8e27
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-15 20:43:57 +0700

    #644 in progress

--- 644 (code prefixes)/644.cpp
+++ 644 (code prefixes)/644.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
 
 //#define ONLINE_JUDGE 1
 
 #define u_int unsigned int
+#define vector std::vector
+#define string std::string
 
 enum t_result {
     e_INVALID_SET = -1,
@@ -10,8 +15,13 @@ enum t_result {
     e_NOT_DECODABLE_SET
 };
 
+const u_int g_DEFAULT_RESERVED_CODES_COUNT = 1000;
+
 void processInput ();
+
 void processTestSet (char & lastSymbol, t_result & result);
+void readTestSetCodes (vector <string> & dictionary, char & lastSymbol);
+
 void printResult (const u_int & index, const t_result & result);
 
 int main ()
@@ -45,7 +55,40 @@ void processInput ()
 
 void processTestSet (char & lastSymbol, t_result & result)
 {
+    vector <string> codeDictionary;
+    codeDictionary.reserve(g_DEFAULT_RESERVED_CODES_COUNT);
 
+    readTestSetCodes(codeDictionary, lastSymbol);
+
+    if (codeDictionary.size() == 0)
+    {
+        result = e_INVALID_SET;
+    }
+    else
+    {
+        result = e_NOT_DECODABLE_SET;//TODO...
+    }
+}
+
+void readTestSetCodes (vector <string> & dictionary, char & lastSymbol)
+{
+    string bufLine = "";
+    std::getline(std::cin, bufLine);
+
+    while (
+            (bufLine.length() > 0)
+            && (bufLine[0] != '9')
+        )
+    {
+        dictionary.push_back(bufLine);
+
+        std::getline(std::cin, bufLine);
+    }
+
+    if (bufLine.length() == 0)
+    {
+        lastSymbol = EOF;
+    }
 }
 
 void printResult (const u_int & index, const t_result & result)
