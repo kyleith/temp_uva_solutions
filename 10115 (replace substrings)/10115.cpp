fec13191df7bf2fbe9632d081989be501d2ff6ac
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-16 21:44:02 +0700

    #10115 in progress

--- 10115 (replace substrings)/10115.cpp
+++ 10115 (replace substrings)/10115.cpp
@@ -1,14 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+#include <queue>
 
 //#define ONLINE_JUDGE 1
 
 #define u_int unsigned int
+#define queue std::queue
+
+const u_int g_MAX_SUBSTRING_LENGTH = 80;
+const u_int g_MAX_TEXT_LENGTH = 255;
+
+struct WordReplacement
+{
+    WordReplacement () { f_word[0] = '\0'; f_replacement[0] = '\0'; }
+    WordReplacement(const char word [], const char replacement [])
+    {
+        strcpy(f_word, word);
+        strcpy(f_replacement, replacement);
+    }
+    WordReplacement (const WordReplacement & copy)
+    {
+        WordReplacement(copy.f_word, copy.f_replacement);
+    }
+    WordReplacement & operator = (const WordReplacement & copy)
+    {
+        strcpy(f_word, copy.f_word);
+        strcpy(f_replacement, copy.f_replacement);
+        return *this;
+    }
+
+    char f_word [g_MAX_SUBSTRING_LENGTH + 1];
+    char f_replacement [g_MAX_SUBSTRING_LENGTH + 1];
+};
+
+struct Text
+{
+    Text () { f_text[0] = '\0'; }
+    Text (const char text []) { strcpy(f_text, text); }
+    Text (const Text & copy) { Text(copy.f_text); }
+    
+    Text & operator = (const Text & copy) { strcpy(f_text, copy.f_text); return *this; }
+    Text & operator = (const char text []) { strcpy(f_text, text); return *this; }
+
+    char f_text [g_MAX_TEXT_LENGTH + 1];
+};
 
 void processTestCase (const u_int & rulesCount);
-void readReplacementsDictionary (const u_int & rulesCount);
-void readInputText ();
-void applyReplacementsInText ();
-void printText ();
+
+void readReplacementsDictionary (const u_int & rulesCount, queue<WordReplacement> & dictionary);
+Text readInputText ();
+void applyReplacementsInText (const queue<WordReplacement> & dictionary, Text & inputText);
+void printText (const Text & inputText);
 
 int main ()
 {
@@ -31,31 +73,36 @@ int main ()
 
 void processTestCase (const u_int & rulesCount)
 {
-    //TODO: store input dictionary in queue<pair<char *, char *>>
+    queue<WordReplacement> dictionary;
 
-    readReplacementsDictionary(rulesCount);
-    readInputText();
-    applyReplacementsInText();
+    readReplacementsDictionary(rulesCount, dictionary);
+    
+    Text currentText = readInputText();
+    
+    applyReplacementsInText(dictionary, currentText);
 
-    printText();
+    printText(currentText);
 }
 
-void readReplacementsDictionary (const u_int & rulesCount)
+void readReplacementsDictionary (const u_int & rulesCount, queue<WordReplacement> & dictionary)
 {
     //TODO...
 }
 
-void readInputText ()
+Text readInputText ()
+{
+    Text inputText;
+
+    //TODO...
+    return inputText;
+}
+
+void applyReplacementsInText (const queue<WordReplacement> & dictionary, Text & inputText)
 {
     //TODO...
 }
 
-void applyReplacementsInText ()
+void printText (const Text & inputText)
 {
-    //TODO...
-}
-
-void printText ()
-{
-    //TODO...
+    printf("%s\n", inputText);
 }
