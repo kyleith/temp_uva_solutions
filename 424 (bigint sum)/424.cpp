3aa19c46b6d80578a3eb8796bacd499bcc9efc18
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-17 21:02:25 +0700

    #424 in progress

--- 424 (bigint sum)/424.cpp
+++ 424 (bigint sum)/424.cpp
@@ -7,6 +7,48 @@
 #define string std::string
 
 const string g_ENDING_LINE = "0";
+const int g_MAX_LONG_NUMBER_LENGTH = 10001;
+
+class BigInt
+{
+    public:
+        BigInt () { f_length = 0; }
+        BigInt (const string & valueString)
+        {
+            f_length = valueString.length();
+            for (int i = 0; i < f_length; i++)
+            {
+                int reverseIndex = f_length - 1 - i;
+                f_digits[i] = valueString[reverseIndex] - '0';
+            }
+        }
+        BigInt(const BigInt & copy)
+        {
+            f_length = copy.f_length;
+            for (int i = 0; i < f_length; i++)
+            {
+                f_digits[i] = copy.f_digits[i];
+            }
+        }
+        BigInt & operator = (const BigInt & copy)
+        {
+            return *this;
+        }
+
+        int m_getLength () { return f_length; }
+        void m_printValue ()
+        {
+            for (int i = f_length - 1; i >= 0; i--)
+            {
+                printf("%d", f_digits[i]);
+            }
+            printf("\n");
+        }
+    
+    private:
+        int f_digits [g_MAX_LONG_NUMBER_LENGTH];
+        int f_length; 
+};
 
 void processInput ();
 
@@ -30,7 +72,8 @@ void processInput ()
             inputString != g_ENDING_LINE
         )
     {
+        BigInt currentNumber (inputString);
+        currentNumber.m_printValue();
         //TODO...
-        std::cout << inputString << std::endl;
     }
 }
