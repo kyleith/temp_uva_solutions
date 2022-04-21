04cff5ef52696669b6f10301368f33aee79d71e2
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-21 21:13:34 +0700

    #465 in progress

--- 465 (int overflow)/465.cpp
+++ 465 (int overflow)/465.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <iostream>
+#include <limits.h>
+#include <string>
+#include <string.h>
 
 //#define ONLINE_JUDGE
+
+#define string std::string
+
+const int g_MAX_INT_VALUE = INT_MAX;
+const char g_OPERATOR_PLUS = '+';
+const char g_OPERATOR_MULT = '*';
+
+void processTestCase (const string & inputStatement);
+void parseStatement (const char * inputStatement);
+void checkStatementWarnings ();
 
 int main ()
 {
@@ -9,5 +23,33 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    string statement;
+    while (std::getline(std::cin, statement))
+    {
+        processTestCase(statement);
+    }
+
     return 0;
+}
+
+void processTestCase (const string & inputStatement)
+{
+    const char * inputSymbols = inputStatement.c_str();
+
+    parseStatement(inputSymbols);
+
+    //TODO: with BigInt, compare each operand with g_MAX_INT_VALUE,
+    //then calculate result and compare it with g_MAX_INT_VALUE
+
+    checkStatementWarnings();
+}
+
+void parseStatement (const char * inputStatement)
+{
+    //TODO: parse to operation, leftValue, rightValue - it is ok to use only BigInt
+}
+
+void checkStatementWarnings ()
+{
+    //TODO: 3 BigInt numbers - left, right and result
 }
