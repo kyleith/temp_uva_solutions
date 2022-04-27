ff668874c65ce83c298660d515a12c994630f30c
hbaglaeva <helenbaglaeva@gmail.com>
2022-04-27 21:03:01 +0700

    #748 in progress

--- 748 (exact presicion multiplication)/748.cpp
+++ 748 (exact presicion multiplication)/748.cpp
@@ -5,6 +5,88 @@
 //#define ONLINE_JUDGE 1
 
 #define string std::string
+#define u_int unsigned int
+
+const u_int g_MAX_BIGINT_LENGTH = 500;
+
+class BigInt
+{
+    public:
+        BigInt ();
+        BigInt (const string & value);
+        BigInt (const BigInt & copy);
+        BigInt & operator = (const BigInt & copy);
+
+        BigInt & operator += (const BigInt & number);
+        BigInt & operator *= (const BigInt & number);
+
+        BigInt operator * (const BigInt & number) const;
+        BigInt & operator *= (const u_int & number);
+        BigInt & operator <<= (const u_int & number);
+    
+    private:
+        char f_digits[g_MAX_BIGINT_LENGTH];//1 byte int
+        u_int f_length;
+};
+
+BigInt::BigInt ()
+{
+    f_length = 0;
+    for (u_int i = 0; i < g_MAX_BIGINT_LENGTH; i++)
+    {
+        f_digits[i] = 0;
+    }
+}
+
+BigInt::BigInt (const string & value)
+{
+    u_int valueLength = value.length();
+    
+    for (u_int i = 0; i < valueLength; i++)
+    {
+        f_digits[i] = value[i] - '0';
+    }
+    for (u_int i = valueLength; i < g_MAX_BIGINT_LENGTH; i++)
+    {
+        f_digits[i] = 0;
+    }
+
+    u_int leadingZeros = 0;
+    for (int i = valueLength - 1; (i >= 0) && (value[i] == '0'); i--)
+    {
+        leadingZeros++;
+    }
+
+    f_length = valueLength - leadingZeros;
+}
+
+BigInt::BigInt (const BigInt & copy)
+{
+    for (u_int i = 0; i < copy.f_length; i++)
+    {
+        f_digits[i] = copy.f_digits[i];
+    }
+    for (u_int i = copy.f_length; i < g_MAX_BIGINT_LENGTH; i++)
+    {
+        f_digits[i] = 0;
+    }
+    f_length = copy.f_length;
+}
+
+BigInt & BigInt::operator = (const BigInt & copy)
+{
+    for (u_int i = 0; i < copy.f_length; i++)
+    {
+        f_digits[i] = copy.f_digits[i];
+    }
+    for (u_int i = copy.f_length; i < g_MAX_BIGINT_LENGTH; i++)
+    {
+        f_digits[i] = 0;
+    }
+    f_length = copy.f_length;
+
+    return *this;
+}
 
 void processInput ();
 void processTestCase (const string & line);
