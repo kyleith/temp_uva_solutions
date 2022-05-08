5613913db364daffb115dc37fbd6a12238a99e90
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-08 19:59:24 +0700

    #10420 in progress

--- 10420 (sorted map)/10420.cpp
+++ 10420 (sorted map)/10420.cpp
@@ -2,6 +2,11 @@
 
 //#define ONLINE_JUDGE 1
 
+void processInput ();
+void processInputLineAndSaveCountry ();
+void saveCountryRepetitionInMap ();
+void printSortedMap ();
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +14,36 @@ int main()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        processInputLineAndSaveCountry();
+        saveCountryRepetitionInMap();
+    }
+
+    printSortedMap();
+}
+
+void processInputLineAndSaveCountry ()
+{
+    //TODO...
+}
+
+void saveCountryRepetitionInMap ()
+{
+    //TODO..
+}
+
+void printSortedMap ()
+{
+    //TODO...
 }
