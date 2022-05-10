90bf596f876a7af035605470795859c1480051b8
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-10 12:11:01 +0700

    #10474 in progress

--- 10474 (qsort and search)/10474.cpp
+++ 10474 (qsort and search)/10474.cpp
@@ -2,6 +2,11 @@
 
 //#define ONLINE_JUDGE
 
+void processInput ();
+void processTestCase (const int & caseIndex, bool & isExit);
+void readArray ();
+void readAndProcessQuery ();
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +14,50 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    bool isExit = false;
+    int caseIndex = 0;
+    while (!isExit)
+    {
+        caseIndex++;
+        processTestCase(caseIndex, isExit);
+    }
+}
+
+void processTestCase (const int & caseIndex, bool & isExit)
+{
+    int n = -1;
+    int q = -1;
+    scanf("%d%d", &n, &q);
+
+    if (n==0 && q==0)
+    {
+        isExit = true;
+        return;
+    }
+    isExit = false;
+
+    printf("CASE# %d:\n", caseIndex);
+
+    readArray();
+    for (int i = 0; i < q; i++)
+    {
+        readAndProcessQuery();
+    }
+}
+
+void readArray ()
+{
+    //TODO...
+}
+
+void readAndProcessQuery ()
+{
+    //TODO...
 }
