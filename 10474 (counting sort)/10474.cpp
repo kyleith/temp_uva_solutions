5d87c74525c3c3c557952a36d81873800d278b49
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-10 14:41:09 +0700

    #10474 solved

--- 10474 (counting sort)/10474.cpp
+++ 10474 (counting sort)/10474.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//#define ONLINE_JUDGE
+#define ONLINE_JUDGE
 
 const int g_ARRAY_LENGTH = 10001;
 
@@ -38,7 +38,10 @@ void processTestCase (const int & caseIndex, bool & isExit)
     int q = -1;
     scanf("%d%d", &n, &q);
 
-    if (n==0 && q==0)
+    if (
+            (n == 0)
+            && (q == 0)
+        )
     {
         isExit = true;
         return;
@@ -66,6 +69,7 @@ void readInputArray (const int & n, int * countArray, int * lowerBoundArray)
         countArray[number]++;
     }
 
+    lowerBoundArray[0] = countArray[0];
     for (int i = 1; i < g_ARRAY_LENGTH; i++)
     {
         lowerBoundArray[i] = lowerBoundArray[i - 1] + countArray[i];
@@ -76,4 +80,13 @@ void readAndProcessQuery (const int * countArray, const int * lowerBoundArray)
 {
     int number = -1;
     scanf("%d", &number);
+
+    if (countArray[number] == 0)
+    {
+        printf("%d not found\n", number);
+    }
+    else
+    {
+        printf("%d found at %d\n", number, lowerBoundArray[number] - countArray[number] + 1);
+    }
 }
