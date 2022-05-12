dca7691a0df419bf7bb50c9ed87a7885995e8fad
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-12 22:18:27 +0700

    #152 in progress

--- 152 (3d points closest neighbours)/152.cpp
+++ 152 (3d points closest neighbours)/152.cpp
@@ -2,6 +2,12 @@
 
 //#define ONLINE_JUDGE 1
 
+void processInput ();
+void readInputPoints ();
+void applyKNearestNeighboursPartition ();
+void saveProcessedResults ();
+void printResults ();
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +15,38 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    readInputPoints();
+
+    //loop until all points are processed
+    applyKNearestNeighboursPartition();
+    saveProcessedResults();
+
+    printResults();
+}
+
+void readInputPoints ()
+{
+    //TODO...
+}
+
+void applyKNearestNeighboursPartition ()
+{
+    //TODO...
+}
+
+void saveProcessedResults ()
+{
+    //TODO...
+}
+
+void printResults ()
+{
+    //TODO...
 }
