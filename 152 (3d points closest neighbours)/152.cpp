7302d079d00196001fba134707b770d136959074
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-13 20:48:15 +0700

    #152 in progress

--- 152 (3d points closest neighbours)/152.cpp
+++ 152 (3d points closest neighbours)/152.cpp
@@ -2,8 +2,18 @@
 
 //#define ONLINE_JUDGE 1
 
+const int g_MAX_POINTS_COUNT = 5000;
+
+struct Point
+{
+    int x = 0;
+    int y = 0;
+    int z = 0;
+};
+
 void processInput ();
-void readInputPoints ();
+
+void readInputPoints (Point * points, int & pointsCount);
 void applyKNearestNeighboursPartition ();
 void saveProcessedResults ();
 void printResults ();
@@ -22,7 +32,10 @@ int main ()
 
 void processInput ()
 {
-    readInputPoints();
+    Point inputPoints[g_MAX_POINTS_COUNT];
+    int n = 0;
+
+    readInputPoints(inputPoints, n);
 
     //loop until all points are processed
     applyKNearestNeighboursPartition();
@@ -31,9 +44,24 @@ void processInput ()
     printResults();
 }
 
-void readInputPoints ()
+void readInputPoints (Point * points, int & pointsCount)
 {
-    //TODO...
+    int x = -1, y = -1, z = -1;
+
+    pointsCount = 0;
+    scanf("%d%d%d", &x, &y, &z);
+    
+    while (x != 0 || y != 0 || z != 0)
+    {
+        Point & pCurrentPoint = points[pointsCount];
+        pCurrentPoint.x = x;
+        pCurrentPoint.y = y;
+        pCurrentPoint.z = z;
+
+        pointsCount++;
+
+        scanf("%d%d%d", &x, &y, &z);
+    }
 }
 
 void applyKNearestNeighboursPartition ()
