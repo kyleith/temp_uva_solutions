7fd25ae3493137a673e0b36d5830ba65fd65b1ee
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-15 19:00:21 +0700

    #299 in progress

--- 299 (sort min steps count)/299.cpp
+++ 299 (sort min steps count)/299.cpp
@@ -2,6 +2,13 @@
 
 //#define ONLINE_JUDGE 1
 
+void processInput ();
+
+void processTestCase ();
+void readTestCaseData ();
+void calculateSwapsNumber ();
+void printTestCaseResult ();
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +16,38 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    //TODO: read N and process test cases in a cycle
+    processTestCase();
+}
+
+void processTestCase ()
+{
+    //TODO...
+    readTestCaseData();
+
+    calculateSwapsNumber();
+
+    printTestCaseResult();
+}
+
+void readTestCaseData ()
+{
+    //TODO: read L and array elements
+}
+
+void calculateSwapsNumber ()
+{
+    //TODO...
+}
+
+void printTestCaseResult ()
+{
+    //TODO...
 }
