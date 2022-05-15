b7a92265d8e4d9718ce61cefedf179b9e8f76baf
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-15 19:34:28 +0700

    #299 incorrect - re-check statement

--- 299 (sort min steps count)/299.cpp
+++ 299 (sort min steps count)/299.cpp
@@ -5,10 +5,13 @@
 const int g_MAX_ARRAY_LENGTH = 50;
 
 void processInput ();
-
 void processTestCase ();
 void readTestCaseData (int * array, int & n);
+
 int calculateSwapsNumber (const int & n, int * array);
+void applyOptimalSorting (const int & n, int * array, int & totalSwaps);
+void swapValues (int & i, int & j, int * array);
+
 void printTestCaseResult (const int & result);
 
 int main ()
@@ -58,11 +61,49 @@ void readTestCaseData (int * array, int & n)
 
 int calculateSwapsNumber (const int & n, int * array)
 {
-    int result = 0;
+    if (n < 2)
+    {
+        return 0;
+    }
 
-    //TODO...
+    int result = 0;
+    applyOptimalSorting(n, array, result);
 
     return result;
+}
+
+void applyOptimalSorting (const int & n, int * array, int & totalSwaps)
+{
+    int currentSwaps = 0;
+    for (int i = 0; i < n; i++)
+    {
+        bool sorted = true;
+        for (int currentIndex = 0; currentIndex < n; currentIndex++)
+        {
+            int correctElementIndex = array[currentIndex] - 1;
+            if (currentIndex != correctElementIndex)
+            {
+                sorted = false;
+
+                swapValues(currentIndex, correctElementIndex, array);
+                currentSwaps++;
+            }
+        }
+
+        if (sorted)
+        {
+            break;
+        }
+    }
+
+    totalSwaps = currentSwaps;
+}
+
+void swapValues (int & i, int & j, int * array)
+{
+    int buffer = array[i];
+    array[i] = array[j];
+    array[j] = buffer;
 }
 
 void printTestCaseResult (const int & result)
