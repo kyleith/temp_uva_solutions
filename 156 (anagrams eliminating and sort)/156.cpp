f55b5e9811d9f2ea010b0ae5241a2a87a68999c7
hbaglaeva <helenbaglaeva@gmail.com>
2022-05-24 19:10:31 +0700

    #156 in progress

--- 156 (anagrams eliminating and sort)/156.cpp
+++ 156 (anagrams eliminating and sort)/156.cpp
@@ -2,6 +2,12 @@
 
 //#define ONLINE_JUDGE
 
+void processInput ();
+void readInputWords ();
+void filterNotAnagrams ();
+void sortResult ();
+void printResult ();
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +15,32 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    //TODO: read words, eliminate anagrams, sort result and print...
+}
+
+void readInputWords ()
+{
+    //TODO...
+}
+
+void filterNotAnagrams ()
+{
+    //TODO...
+}
+
+void sortResult ()
+{
+    //TODO...
+}
+
+void printResult ()
+{
+    //TODO...
 }
