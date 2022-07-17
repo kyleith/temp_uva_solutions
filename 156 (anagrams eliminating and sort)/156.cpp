a41532c000118b994abf4c2780c107f5ec1b8794
hbaglaeva <helenbaglaeva@gmail.com>
2022-07-17 14:28:25 +0700

    #156 solved AC

--- 156 (anagrams eliminating and sort)/156.cpp
+++ 156 (anagrams eliminating and sort)/156.cpp
@@ -5,15 +5,18 @@
 #include <vector>
 #include <cctype>
 #include <algorithm>
+#include <utility>
 
-//#define ONLINE_JUDGE
+#define ONLINE_JUDGE 1
 
 #define string std::string
 #define vector std::vector
 #define map std::map
 
-#define t_wordsVector vector <string>
+#define t_wordsVector vector <std::pair<string, string>>
 #define t_wordsMap map <string, int>
+
+const int g_SINGLE_REPETITION_VALUE = 1;
 
 void processInput ();
 
@@ -21,7 +24,7 @@ void readAndPreFilterWords (t_wordsVector & wordsCandidates, t_wordsMap & wordsM
 string transformWordToMapKey (const string & word);
 
 void sortWordsVector (t_wordsVector & words);
-void printResult (const t_wordsVector & wordsCandidates, const t_wordsMap & wordsMap);
+void printResult (const t_wordsVector & wordsCandidates, t_wordsMap & wordsMap);
 
 int main ()
 {
@@ -65,8 +68,8 @@ void readAndPreFilterWords (t_wordsVector & wordsCandidates, t_wordsMap & wordsM
         bool isKeyFound = (foundElement != wordsMap.end());
         if (!isKeyFound)
         {
-            wordsMap[currentKey] = 0;
-            wordsCandidates.push_back(currentWord);
+            wordsMap[currentKey] = g_SINGLE_REPETITION_VALUE;
+            wordsCandidates.push_back(std::make_pair(currentWord, currentKey));
         }
         else
         {
@@ -94,14 +97,21 @@ string transformWordToMapKey (const string & word)
 
 void sortWordsVector (t_wordsVector & words)
 {
+    //default sorting of std::pair<string, string> uses first string - this is intended
     std::sort(words.begin(), words.end());
 }
 
-void printResult (const t_wordsVector & wordsCandidates, const t_wordsMap & wordsMap)
+void printResult (const t_wordsVector & wordsCandidates, t_wordsMap & wordsMap)
 {
     int wordsCount = wordsCandidates.size();
     for (int i = 0; i < wordsCount; i++)
     {
-        //TODO...
+        string currentKey = wordsCandidates[i].second;
+        
+        bool isUniqueKey = (wordsMap[currentKey] == g_SINGLE_REPETITION_VALUE);
+        if (isUniqueKey)
+        {
+            std::cout << wordsCandidates[i].first << "\n";
+        }
     }
 }
