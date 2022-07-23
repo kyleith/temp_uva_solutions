300ad528d461e147497d42f648f38ae70ee2633c
hbaglaeva <helenbaglaeva@gmail.com>
2022-07-23 18:41:40 +0700

    #400 in progress

--- 400 (unix ls)/400.cpp
+++ 400 (unix ls)/400.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
 //#define ONLINE_JUDGE 1
 
+#define string std::string
+#define vector std::vector
+
 void processInput ();
+void processTestCase (bool & isEndCase);
+
+void readFilenames (const int & n, vector<string> & list, int & longestLength);
+void sortFilenames (vector<string> & list);
+void printFormattedResult (const vector<string> & list, const int & longestLength);
 
 int main ()
 {
@@ -17,5 +28,43 @@ int main ()
 
 void processInput ()
 {
+    bool isEndCase = false;
+    while (!isEndCase)
+    {
+        processTestCase(isEndCase);
+    }
+}
+
+void processTestCase (bool & isEndCase)
+{
+    int n = 0;
+
+    if (std::cin >> n)
+    {
+        vector<string> filenames;
+        int longestLength = 0;
+
+        readFilenames(n, filenames, longestLength);
+        sortFilenames(filenames);
+        printFormattedResult(filenames, longestLength);
+    }
+    else
+    {
+        isEndCase = true;
+    }
+}
+
+void readFilenames (const int & n, vector<string> & list, int & longestLength)
+{
     //TODO...
+}
+
+void sortFilenames (vector<string> & list)
+{
+    //TODO...
+}
+
+void printFormattedResult (const vector<string> & list, const int & longestLength)
+{
+
 }
