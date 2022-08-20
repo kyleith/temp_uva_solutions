30f25645c118b778870cbd3b3d45736eeefc9d99
hbaglaeva <helenbaglaeva@gmail.com>
2022-08-20 13:20:42 +0700

    #123 in progress

--- 123 (sort by keywords)/123.cpp
+++ 123 (sort by keywords)/123.cpp
@@ -2,18 +2,31 @@
 #include <iostream>
 #include <cstring>
 #include <set>
+#include <map>
+#include <vector>
+#include <utility>
 
 //#define ONLINE_JUDGE 1
 
 #define string std::string
 #define set std::set
+#define map std::map
+#define vector std::vector
+#define pair std::pair
+#define t_keywordPair pair<string, vector<string>>
 
 void processInput ();
 
 void readListOfWords (set<string> & words);
-void readAndProcessListOfTitles (const set<string> & ignorableWords);
-void populateKeywordsFromTitle (const string & title);
-void printSortedResult ();
+void readAndProcessListOfTitles (
+        const set<string> & ignorableWords,
+        map<string, t_keywordPair> & dictionary
+    );
+void populateKeywordsFromTitle (
+        const string & title,
+        map<string, t_keywordPair> & dictionary
+    );
+void printSortedResult (const map<string, t_keywordPair> & dictionary);
 
 int main ()
 {
@@ -30,10 +43,11 @@ int main ()
 void processInput ()
 {
     set<string> ignorableWords;
+    map<string, t_keywordPair> kwicDictionary;
 
     readListOfWords(ignorableWords);
-    readAndProcessListOfTitles(ignorableWords);
-    printSortedResult();
+    readAndProcessListOfTitles(ignorableWords, kwicDictionary);
+    printSortedResult(kwicDictionary);
 }
 
 void readListOfWords (set<string> & words)
@@ -41,18 +55,24 @@ void readListOfWords (set<string> & words)
     //TODO: read list of ignorable words, ending is "::"
 }
 
-void readAndProcessListOfTitles (const set<string> & ignorableWords)
+void readAndProcessListOfTitles (
+        const set<string> & ignorableWords,
+        map<string, t_keywordPair> & dictionary
+    )
 {
     //TODO: read list of titles one by line, ending is EOF.
-    //Then instead of storing all the titles, process each title immediaterly.
+    //Then instead of storing all the titles, process each title immediately.
 }
 
-void populateKeywordsFromTitle (const string & title)
+void populateKeywordsFromTitle (
+        const string & title,
+        map<string, t_keywordPair> & dictionary
+    )
 {
-    //TODO: detect keywords, capitilize and put their sentences to sorted dictionary
+    //TODO: detect keywords, capitalize and put their sentences to sorted dictionary
 }
 
-void printSortedResult ()
+void printSortedResult (const map<string, t_keywordPair> & dictionary)
 {
     //TODO: print KWIC-index, i.e. print related sentences of sorted keywords from the dictionary
 }
