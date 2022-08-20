e87009ee6724a3a09bf302ac80f0c9ca7bb169a8
hbaglaeva <helenbaglaeva@gmail.com>
2022-08-20 11:49:41 +0700

    #123 in progress

--- 123 (sort by keywords)/123.cpp
+++ 123 (sort by keywords)/123.cpp
@@ -2,6 +2,13 @@
 
 //#define ONLINE_JUDGE 1
 
+void processInput ();
+
+void readListOfWords ();
+void readListOfTitles ();
+void populateKeywords ();
+void printSortedResult ();
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +16,35 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    readListOfWords();
+    readListOfTitles();
+    populateKeywords();
+    printSortedResult();
+}
+
+void readListOfWords ()
+{
+    //TODO: read list of ignorable words, ending is "::"
+}
+
+void readListOfTitles ()
+{
+    //TODO: read list of titles one by line, ending is EOF
+}
+
+void populateKeywords ()
+{
+    //TODO: detect keywords, capitilize and put their sentences to sorted dictionary
+}
+
+void printSortedResult ()
+{
+    //TODO: print KWIC-index, i.e. print related sentences of sorted keywords from the dictionary
 }
