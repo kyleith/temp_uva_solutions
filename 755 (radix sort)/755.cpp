2c914176ba7aedf9298bba1ea26c1711629859c6
hbaglaeva <helenbaglaeva@gmail.com>
2022-11-30 12:19:01 +0700

    #755 in progress

--- 755 (radix sort)/755.cpp
+++ 755 (radix sort)/755.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
 
 //#define ONLINE_JUDGE 1
+
+#define string std::string
+
+void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -9,5 +16,34 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    int testCasesCount = 0;
+    std::cin >> testCasesCount;
+
+    processTestCase();
+    for (int i = 1; i < testCasesCount; i++)
+    {
+        std::cout << "\n";
+        processTestCase();
+    }
+}
+
+void processTestCase ()
+{
+    int numbersCount = 0;
+    std::cin >> numbersCount;
+
+    string line;
+    std::getline(std::cin, line);/*read line ending*/
+
+    for (int i = 0; i < numbersCount; i++)
+    {
+        std::getline(std::cin, line);
+    }
 }
