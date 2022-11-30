90dd86fa67028934e0aa09111063de7110a01267
hbaglaeva <helenbaglaeva@gmail.com>
2022-11-30 16:21:47 +0700

    #10785 in progress

--- 10785 (encode smallest string)/10785.cpp
+++ 10785 (encode smallest string)/10785.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
 
 //#define ONLINE_JUDGE 1
+
+#define string std::string
+
+void processInput ();
+string calculateName (const int & length);
 
 int main ()
 {
@@ -9,5 +16,28 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    int testCasesCount = 0;
+    std::cin >> testCasesCount;
+
+    for (int i = 1; i <= testCasesCount; i++)
+    {
+        int length = 0;
+        std::cin >> length;
+
+        string result = calculateName(length);
+        std::cout << "Case " << i << ": " << result << "\n";
+    }
+}
+
+string calculateName (const int & length)
+{
+    //TODO...
+    return "";
 }
