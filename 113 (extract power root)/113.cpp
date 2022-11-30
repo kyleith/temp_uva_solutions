6111a71e9fcd91d000ab63dad26db6553a2b47e5
hbaglaeva <helenbaglaeva@gmail.com>
2022-11-30 20:21:29 +0700

    #113 accepted with pow

--- 113 (extract power root)/113.cpp
+++ 113 (extract power root)/113.cpp
@@ -22,7 +22,8 @@ void processInput()
     long double n = 0, p = 0;
     while (scanf("%Lf%Lf", &n, &p) == 2)
     {
-        long double result = exp(log(p) / n);
+        //long double result = exp(log(p) / n); - this is correct math solution
+        long double result = pow(p, 1.0/n);
         printf("%.0Lf\n", result);
     }
 }
