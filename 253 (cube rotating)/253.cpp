6d833c332c7835bbd863e638855cc070f7729841
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-03 18:23:19 +0700

    #253 in progress

--- 253 (cube rotating)/253.cpp
+++ 253 (cube rotating)/253.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
 
 //#define ONLINE_JUDGE 1
+
+#define string std::string
+
+void processInput ();
+void processCase (const string & input);
 
 int main ()
 {
@@ -9,5 +16,21 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    string inputString;
+    while (std::getline(std::cin, inputString))
+    {
+        processCase(inputString);
+    }
+}
+
+void processCase (const string & input)
+{
+    //TODO: decode input, build cubes and compare cubes
 }
