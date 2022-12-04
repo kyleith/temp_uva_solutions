4f33181533a992275d45b4bae9b8c82bfd81fc7c
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-04 19:38:32 +0700

    #10025 in progress

--- 10025 (triangular numbers)/10025.cpp
+++ 10025 (triangular numbers)/10025.cpp
@@ -2,6 +2,9 @@
 
 //#define ONLINE_JUDGE 1
 
+void processInput ();
+void processCase (const long & k);
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +12,26 @@ int main ()
     freopen("output.txt", "wt", stdout);
 #endif
 
+    processInput();
+
     return 0;
+}
+
+void processInput ()
+{
+    long k;
+    if (scanf("%ld", &k) == 1)
+    {
+        processCase(k);
+    }
+    while (scanf("%ld", &k) == 1)
+    {
+        printf("\n");
+        processCase(k);
+    }
+}
+
+void processCase (const long & k)
+{
+    //TODO...
 }
