3ae7bc230f50206c25b64ed87542eacd7d81e4f7
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-11 20:03:17 +0700

    #107 in progress

--- 107 (binsearch by formula)/107.cpp
+++ 107 (binsearch by formula)/107.cpp
@@ -1,6 +1,11 @@
 #include <cstdio>
+#include <cmath>
 
 //#define ONLINE_JUDGE 1
+
+void processInput ();
+void processCase (const long long & H0, const long long & CLast);
+long long binSearch (const long long & left, const long long & right, const long long & eqValue);
 
 int main ()
 {
@@ -9,5 +14,36 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
+	return 0;
+}
+
+void processInput ()
+{
+	long long H0, CLast;
+	scanf("%lld%lld", &H0, &CLast);
+
+	bool isValidCase = ((H0 != 0) && (CLast != 0));
+	while (isValidCase)
+	{
+		processCase(H0, CLast);
+
+		scanf("%lld%lld", &H0, &CLast);
+		isValidCase = ((H0 != 0) && (CLast != 0));
+	}
+}
+
+void processCase (const long long & H0, const long long & CLast)
+{
+	long long N = binSearch(0, H0, CLast);
+	long long last = log(CLast)/log(N);
+
+	//TODO...
+}
+
+long long binSearch (const long long & left, const long long & right, const long long & eqValue)
+{
+	//TODO...
 	return 0;
 }
