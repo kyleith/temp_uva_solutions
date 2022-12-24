77edc5dc7a115f0bd62419e56af52188c0ae9f7b
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-24 22:15:58 +0700

    #10499 solved AC & readme

--- 10499 (simple formula)/10499.cpp
+++ 10499 (simple formula)/10499.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 
-//#define ONLINE_JUDGE 1
+#define ONLINE_JUDGE 1
+
+void processInput ();
+void processTestCase (const long long & n);
 
 int main ()
 {
@@ -9,5 +12,34 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	long long n;
+	scanf("%lld", &n);
+
+	while (n >= 0)
+	{
+		processTestCase(n);
+
+		scanf("%lld", &n);
+	}
+}
+
+void processTestCase (const long long & n)
+{
+	long long result = -1;
+	if (n <= 1)
+	{
+		result = 0;
+	}
+	else
+	{
+		result = 50 + (25 * (n - 2));
+	}
+	printf("%lld%\n", result);
 }
