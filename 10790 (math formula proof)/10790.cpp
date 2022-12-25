c874e08f95d0d09a0f0ae1fda81af5908cc67761
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-25 14:05:54 +0700

    #10790 solved AC

--- 10790 (math formula proof)/10790.cpp
+++ 10790 (math formula proof)/10790.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 
-//#define ONLINE_JUDGE 1
+#define ONLINE_JUDGE 1
+
+void processInput ();
+void processTestCase (const int & caseIndex, const long long & a, const long long & b);
 
 int main ()
 {
@@ -9,5 +12,31 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	long long a, b;
+	scanf("%lld%lld", &a, &b);
+
+	bool isValidInput = ((a > 0) && (b > 0));
+	int caseIndex = 0;
+	while (isValidInput)
+	{
+		caseIndex++;
+		processTestCase(caseIndex, a, b);
+
+		scanf("%lld%lld", &a, &b);
+		isValidInput = ((a > 0) && (b > 0));
+	}
+
+}
+
+void processTestCase (const int & caseIndex, const long long & a, const long long & b)
+{
+	long long result = a * (a - 1) * b * (b - 1) / 4;
+	printf("Case %d: %lld\n", caseIndex, result);
 }
