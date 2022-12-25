bc40b2462218cc99ae1c1a31bc5e8c838ce15025
hbaglaeva <helenbaglaeva@gmail.com>
2022-12-25 15:30:42 +0700

    #11044 solved AC

--- 11044 (simple formula)/11044.cpp
+++ 11044 (simple formula)/11044.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
+#include <cmath>
 
-//#define ONLINE_JUDGE 1
+#define ONLINE_JUDGE 1
+
+void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -9,5 +13,27 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	long n, m;
+	scanf("%ld%ld", &n, &m);
+
+	long result = ceil((n - 2.0) / 3.0) * ceil((m - 2.0) / 3.0);
+	printf("%ld\n", result);
 }
