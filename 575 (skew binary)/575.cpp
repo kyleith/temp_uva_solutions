63b5395dc9fe2e9251b9be47b0c54af5ac605b7c
hbaglaeva <helenbaglaeva@gmail.com>
2023-01-14 21:29:36 +0700

    #575 solved AC & readme

--- 575 (skew binary)/575.cpp
+++ 575 (skew binary)/575.cpp
@@ -1,6 +1,14 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
-//#define ONLINE_JUDGE 1
+#define ONLINE_JUDGE 1
+
+#define string std::string
+
+void processInput ();
+void processTestCase (const string & inputNumber);
 
 int main ()
 {
@@ -9,5 +17,31 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	string inputNumber;
+	while (
+			std::getline(std::cin, inputNumber)
+			&& (inputNumber != "0")
+		)
+	{
+		processTestCase(inputNumber);
+	}
+}
+
+void processTestCase (const string & inputNumber)
+{
+	long long result = 0, currentPow = 1;
+	for (int i = inputNumber.length() - 1; i >= 0; i--)
+	{
+		currentPow <<= 1;
+		result += ((inputNumber[i] - '0') * (currentPow - 1));
+	}
+
+	printf("%lld\n", result);
 }
---
+++ 575 (skew binary)/readme.txt
@@ -0,0 +1,2 @@
+575 - Skew Binary
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=100&page=show_problem&problem=516
