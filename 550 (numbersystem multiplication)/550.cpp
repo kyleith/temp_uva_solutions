228c0776e53e35362de8a473ca1b7d10f2b387e2
hbaglaeva <helenbaglaeva@gmail.com>
2023-01-21 22:01:27 +0700

    #550 solved AC & readme

--- 550 (numbersystem multiplication)/550.cpp
+++ 550 (numbersystem multiplication)/550.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
 
+#define ONLINE_JUDGE
+
+void processInput ();
+void processTestCase (const int & base, const int & initialDigit, const int & secondFactor);
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -7,5 +12,32 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	int base, digit, factor;
+	while (scanf("%d%d%d", &base, &digit, &factor) == 3)
+	{
+		processTestCase(base, digit, factor);
+	}
+}
+
+void processTestCase (const int & base, const int & initialDigit, const int & secondFactor)
+{
+	int currentDigit = initialDigit;
+	int carrier = 0;
+	int numberLength = 1;
+	int temp = 0;
+	while ( (temp = (currentDigit * secondFactor) + carrier) != initialDigit)
+	{
+		currentDigit = temp % base;
+		carrier = temp / base;
+		numberLength++;
+	}
+
+	printf("%d\n", numberLength);
 }
---
+++ 550 (numbersystem multiplication)/readme.txt
@@ -0,0 +1,2 @@
+550 - Multiplying by Rotation
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=100&page=show_problem&problem=491
