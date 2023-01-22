5c30b89eb481158470a2d0b66e5776397cbc49f9
hbaglaeva <helenbaglaeva@gmail.com>
2023-01-22 15:53:22 +0700

    #568 solved AC & readme

--- 568 (factorials)/568.cpp
+++ 568 (factorials)/568.cpp
@@ -1,11 +1,14 @@
 #include <cstdio>
 
-//#define ONLINE_JUDGE
+#define ONLINE_JUDGE
 
 const int g_MAX_N_VALUE = 10000;
 int g_resultDigits [g_MAX_N_VALUE + 1] = {0};
 
 void initResultsArray ();
+void trimEndingZeros (long long & value);
+void trimNumberLength (long long & number);
+
 int getResultDigit (const int & n);
 
 void processInput ();
@@ -26,7 +29,30 @@ int main ()
 
 void initResultsArray ()
 {
-	//TODO...
+	g_resultDigits[0] = 1;// "0! = 1"
+
+	long long buf = 1;
+	for (int i = 1; i <= g_MAX_N_VALUE; i++)
+	{
+		buf *= i;
+		trimEndingZeros(buf);
+		g_resultDigits[i] = buf % 10;
+
+		trimNumberLength(buf);
+	}
+}
+
+void trimEndingZeros (long long & value)
+{
+	while (value % 10 == 0)
+	{
+		value /= 10;
+	}
+}
+
+void trimNumberLength (long long & number)
+{
+	number %= 100000000; // 8 digits in result
 }
 
 int getResultDigit (const int & n)
