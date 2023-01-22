ff23d613da7aaf78eecceab772b3d43faf9a979f
hbaglaeva <helenbaglaeva@gmail.com>
2023-01-22 15:30:45 +0700

    #568 in progress

--- 568 (factorials)/568.cpp
+++ 568 (factorials)/568.cpp
@@ -2,6 +2,15 @@
 
 //#define ONLINE_JUDGE
 
+const int g_MAX_N_VALUE = 10000;
+int g_resultDigits [g_MAX_N_VALUE + 1] = {0};
+
+void initResultsArray ();
+int getResultDigit (const int & n);
+
+void processInput ();
+void processTestCase (const int & n);
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -9,5 +18,39 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	initResultsArray();
+	processInput();
+
 	return 0;
+}
+
+void initResultsArray ()
+{
+	//TODO...
+}
+
+int getResultDigit (const int & n)
+{
+	return g_resultDigits[n];
+}
+
+void processInput ()
+{
+	int n;
+	while (scanf("%d", &n) == 1)
+	{
+		processTestCase(n);
+	}
+}
+
+void processTestCase (const int & n)
+{
+	if (n == 0)
+	{
+		printf("    0 -> 1\n");
+		return;
+	}
+
+	int digit = getResultDigit(n);
+	printf("%5.d -> %d\n", n, digit);
 }
