342f36095a884ea5c2252ad780081027fa8036c9
hbaglaeva <helenbaglaeva@gmail.com>
2023-01-29 17:44:48 +0700

    #408 solved AC & readme

--- 408 (gcd)/408.cpp
+++ 408 (gcd)/408.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
+#include <algorithm>
 
-//#define ONLINE_JUDGE
+void processInput ();
+void processTestCase (const long & step, const long & mod);
+long gcd (const long & a, const long & b);
 
 int main ()
 {
@@ -9,5 +12,47 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	long step, mod;
+	while (scanf("%ld%ld", &step, &mod) == 2)
+	{
+		processTestCase(step, mod);
+	}
+}
+
+void processTestCase (const long & step, const long & mod)
+{
+	bool isGoodChoice = (gcd(step, mod) == 1);
+
+	printf("%10.ld%10.ld    ", step, mod);
+	if (isGoodChoice)
+	{
+		printf("Good Choice\n\n");
+	}
+	else
+	{
+		printf("Bad Choice\n\n");
+	}
+}
+
+long gcd (const long & a, const long & b)
+{
+	long valueMin = std::min(a, b);
+	long valueMax = std::max(a, b);
+
+	while (valueMin > 0)
+	{
+		long buffer = valueMin;
+
+		valueMin = valueMax % valueMin;
+		valueMax = buffer;
+	}
+
+	return valueMax;
 }
