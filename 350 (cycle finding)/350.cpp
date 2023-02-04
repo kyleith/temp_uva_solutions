3ac7b530ec267cd0d7d685ab9995515fb36af95e
hbaglaeva <helenbaglaeva@gmail.com>
2023-02-04 22:12:28 +0700

    #350 solved AC & readme

--- 350 (cycle finding)/350.cpp
+++ 350 (cycle finding)/350.cpp
@@ -1,5 +1,9 @@
 #include <cstdio>
 
+void processInput ();
+void processTestCase (const long & caseIndex, const long long & z, const long long & i, const long long & m, const long long & l);
+long long calculateFormula (const long long & z, const long long & i, const long long & m, const long long & l);
+
 int main ()
 {
 #ifndef ONLINE_JUDGE
@@ -7,5 +11,63 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
+	processInput();
+
 	return 0;
+}
+
+void processInput ()
+{
+	long caseIndex = 0;
+	long long z, i, m, l;
+
+	scanf("%lld%lld%lld%lld", &z, &i, &m, &l);
+	bool isEnding = (z==0 && i==0 && m==0 && l==0);
+
+	while (!isEnding)
+	{
+		caseIndex++;
+		processTestCase(caseIndex, z, i, m, l);
+
+		scanf("%lld%lld%lld%lld", &z, &i, &m, &l);
+		isEnding = (z==0 && i==0 && m==0 && l==0);
+	}
+}
+
+void processTestCase (const long & caseIndex, const long long & z, const long long & i, const long long & m, const long long & l)
+{
+	long long tortoisePos = l, harePos = l;
+	long long vPos;/*meeting point*/
+	tortoisePos = calculateFormula(z, i, m, tortoisePos);
+	harePos = calculateFormula(z, i, m, calculateFormula(z, i, m, harePos));
+	while (tortoisePos != harePos)
+	{
+		tortoisePos = calculateFormula(z, i, m, tortoisePos);
+		harePos = calculateFormula(z, i, m, calculateFormula(z, i, m, harePos));
+	}
+	vPos = harePos;
+
+	long long muPos;/*beginning of the cycle*/
+	tortoisePos = l;
+	while (tortoisePos != harePos)
+	{
+		tortoisePos = calculateFormula(z, i, m, tortoisePos);
+		harePos = calculateFormula(z, i, m, harePos);
+	}
+	muPos = harePos;
+
+	long lambda = 1;/*cycle length*/
+	harePos = calculateFormula(z, i, m, harePos);
+	while (tortoisePos != harePos)
+	{
+		lambda++;
+		harePos = calculateFormula(z, i, m, harePos);
+	}
+
+	printf("Case %ld: %ld\n", caseIndex, lambda);
+}
+
+long long calculateFormula (const long long & z, const long long & i, const long long & m, const long long & l)
+{
+	return (z * l + i) % m;
 }
