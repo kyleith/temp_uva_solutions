fb8608f05a7d56a295e2efc2905e059f98bb7d03
hbaglaeva <helenbaglaeva@gmail.com>
2023-02-12 17:55:12 +0700

    #10879 solved AC & readme

--- 10879 (brutforce)/10879.cpp
+++ 10879 (brutforce)/10879.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
+#include <cmath>
 
 void processInput();
+void processTestCase (const int & caseIndex, const long & k);
 
 int main ()
 {
@@ -9,12 +11,49 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
-	void processInput();
+	processInput();
 
 	return 0;
 }
 
 void processInput()
 {
-	//TODO...
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		long k;
+		scanf("%ld", &k);
+
+		processTestCase(i, k);
+	}
+}
+
+void processTestCase (const int & caseIndex, const long & k)
+{
+	long a, b, c, d;
+	int matchesCount = 0;
+
+	long root = sqrt(k);
+	for (long i = 2; (i <= root) && (matchesCount < 2); i++)
+	{
+		if (k % i == 0)
+		{
+			matchesCount++;
+
+			if (matchesCount == 1)
+			{
+				a = i;
+				b = k / i;
+			}
+			else
+			{
+				c = i;
+				d = k / i;
+			}
+		}
+	}
+
+	printf("Case #%d: %ld = %ld * %ld = %ld * %ld\n", caseIndex, k, a, b, c, d);
 }
---
+++ 10879 (brutforce)/readme.txt
@@ -0,0 +1,2 @@
+10879 - Code Refactoring
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=100&page=show_problem&problem=1820
