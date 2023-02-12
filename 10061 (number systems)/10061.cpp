6426efae2df8962480c174c9195e84b683879fda
hbaglaeva <helenbaglaeva@gmail.com>
2023-02-12 12:22:04 +0700

    #10061 in progress

--- 10061 (number systems)/10061.cpp
+++ 10061 (number systems)/10061.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 
 void processInput ();
+void processTestCase (const long & number, const long & base);
+
+long calculateZeros (const long & number, const long & base);
+long calculateDigits (const long & number, const long & base);
 
 int main ()
 {
@@ -16,5 +20,30 @@ int main ()
 
 void processInput ()
 {
+	long n, b;
+
+	while (scanf("%ld%ld", &n, &b) == 2)
+	{
+		processTestCase(n, b);
+	}
+}
+
+void processTestCase (const long & number, const long & base)
+{
+	long zerosCount = calculateDigits(number, base);
+	long digitsCount = calculateDigits(number, base);
+
+	printf("%ld %ld\n", zerosCount, digitsCount);
+}
+
+long calculateZeros (const long & number, const long & base)
+{
 	//TODO...
+	return 0;
+}
+
+long calculateDigits (const long & number, const long & base)
+{
+	//TODO...
+	return 0;
 }
