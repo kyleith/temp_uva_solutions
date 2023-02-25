d367e7749f1790602909884cf2b171462ee08f30
hbaglaeva <helenbaglaeva@gmail.com>
2023-02-25 16:34:16 +0700

    #10387 solved AC & readme

--- 10387 (ray reflection inside rectangular)/10387.cpp
+++ 10387 (ray reflection inside rectangular)/10387.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
+#include <cmath>
 
 void processInput ();
+void processTestCase (const int & a, const int & b, const int & s, const int & m, const int & n);
 
 int main ()
 {
@@ -16,5 +18,24 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int a, b, s, m, n;
+	scanf("%d%d%d%d%d", &a, &b, &s, &m, &n);
+
+	while (a>0 && b>0 && s>0 && m>0 && n>0)
+	{
+		processTestCase(a, b, s, m, n);
+
+		scanf("%d%d%d%d%d", &a, &b, &s, &m, &n);
+	}
+}
+
+void processTestCase (const int & a, const int & b, const int & s, const int & m, const int & n)
+{
+	double height = b * n;
+	double width = a * m;
+
+	double angle = atan(height / width) * 180.0 / M_PI;
+	double velocity = sqrt(height * height + width * width) / s;
+
+	printf("%.2lf %.2lf\n", angle, velocity);
 }
---
+++ 10387 (ray reflection inside rectangular)/readme.txt
@@ -0,0 +1,2 @@
+10387 - Billiard
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=101&page=show_problem&problem=1328
