f03a8de0ee228fe14a37b56b32978bc4425de9d1
hbaglaeva <helenbaglaeva@gmail.com>
2023-03-19 12:32:49 +0700

    #101 in progress

--- 101 (commands on 2d array)/101.cpp
+++ 101 (commands on 2d array)/101.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
+#include <vector>
+
+#define vector std::vector
 
 void processInput ();
+void initStacks (const int & n, vector<vector<int>> & stacks);
 
 int main ()
 {
@@ -16,5 +20,24 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	vector<vector<int>> stacks;
+	int n = -1;
+	scanf("%d\n", &n);
+
+	initStacks(n, stacks);
+
+	//TODO: commands
+}
+
+void initStacks (const int & n, vector<vector<int>> & stacks)
+{
+	stacks.reserve(n);
+	for (int i = 0; i < n; i++)
+	{
+		vector<int> temp;
+		temp.reserve(n);
+		temp.push_back(i);
+
+		stacks.push_back(temp);
+	}
 }
