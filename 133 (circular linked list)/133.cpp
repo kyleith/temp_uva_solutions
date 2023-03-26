dc37b5285067dc5df5c7e356e95812c6869c994a
hbaglaeva <helenbaglaeva@gmail.com>
2023-03-26 16:36:25 +0700

    #133 in progress

--- 133 (circular linked list)/133.cpp
+++ 133 (circular linked list)/133.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <vector>
+
+#define vector std::vector
 
 void processInput ();
 void processTestCase (const int & n, const int & k, const int & m);
@@ -24,11 +27,87 @@ void processInput ()
 	)
 	{
 		processTestCase(n, k, m);
-		/* code */
 	}
 }
 
 void processTestCase (const int & n, const int & k, const int & m)
 {
-	//TODO...
+	vector<int> list;
+	list.reserve(n);
+	for (int i = 1; i <= n; i++)
+	{
+		list.push_back(i);
+	}
+
+	int lastKIndex = 0;
+	int lastMIndex = n - 1;
+	while (list.size() > 0)
+	{
+		int listLength = list.size();
+		int nextKIndex = (lastKIndex + k) % listLength;
+		int nextMIndex = (listLength + lastMIndex - (m % listLength)) % listLength;
+
+		if (nextKIndex == nextMIndex)
+		{
+			printf("%3.d", list[nextKIndex]);
+
+			list.erase(list.begin() + nextKIndex);
+
+			listLength = list.size();
+
+			if (nextKIndex == listLength)
+			{
+				nextKIndex = 0;
+			}
+
+			nextMIndex--;/*update M counter*/
+			if (nextMIndex < 0)
+			{
+				nextMIndex = listLength - 1;
+			}
+		}
+		else
+		{
+			printf("%3.d%3.d", list[nextKIndex], list[nextMIndex]);
+
+			//remove K counter...
+			list.erase(list.begin() + nextKIndex);
+			if (nextMIndex > nextKIndex)
+			{
+				nextMIndex--;
+			}
+
+			if (nextKIndex == list.size())
+			{
+				nextKIndex = 0;
+			}
+
+			//remove M counter...
+			list.erase(list.begin() + nextMIndex);
+			if (nextKIndex > nextMIndex)
+			{
+				nextKIndex--;
+			}
+			nextMIndex--;/*update M counter*/
+
+			listLength = list.size();
+			if (nextKIndex == listLength)
+			{
+				nextKIndex = 0;
+			}
+			if (nextMIndex < 0)
+			{
+				nextMIndex = listLength - 1;
+			}
+		}
+
+		lastKIndex = nextKIndex;
+		lastMIndex = nextMIndex;
+
+		if (list.size() > 0)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
 }
