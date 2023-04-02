8f97fa1a26a3fa6f2db165539445a33de2bce16a
hbaglaeva <helenbaglaeva@gmail.com>
2023-04-02 14:20:16 +0700

    #10152 in progress

--- 10152 (sorting)/10152.cpp
+++ 10152 (sorting)/10152.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#define vector std::vector
+#define string std::string
 
 void processInput ();
+void processTestCase ();
+void readStack (const int & n, vector<string> & stack);
+void calculateAndPrintMoves (const vector<string> & leftStack, const vector<string> & rightStack);
 
 int main ()
 {
@@ -15,6 +24,46 @@ int main ()
 }
 
 void processInput ()
+{
+	int k = -1;
+	scanf("%d\n", &k);
+
+	for (int i = 0; i < k; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	int n;
+	vector<string> leftStack;
+	vector<string> rightStack;
+
+	scanf("%d\n", &n);
+	readStack(n, leftStack);
+	readStack(n, rightStack);
+	calculateAndPrintMoves(leftStack, rightStack);
+
+	printf("\n");
+}
+
+void readStack (const int & n, vector<string> & stack)
+{
+	stack.reserve(n);
+	for (int i = 0; i < n; i++)
+	{
+		stack.push_back("");
+	}
+	for (int i = 0; i < n; i++)
+	{
+		string line;
+		std::getline(std::cin, line);
+		stack[n - 1 - i] = line;
+	}
+}
+
+void calculateAndPrintMoves (const vector<string> & leftStack, const vector<string> & rightStack)
 {
 	//TODO...
 }
