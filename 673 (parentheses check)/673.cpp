da12f7edcc49e317186604b93889ce7b3da5fbc9
hbaglaeva <helenbaglaeva@gmail.com>
2023-04-09 18:27:25 +0700

    #673 solved AC & readme

--- 673 (parentheses check)/673.cpp
+++ 673 (parentheses check)/673.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include <stack>
 
 #define string std::string
+#define stack std::stack
 
 void processInput ();
 void processTestCase ();
@@ -49,6 +51,56 @@ void processTestCase ()
 
 bool testBrackets (const string & line)
 {
-	//TODO...
-	return false;
+	stack<char> buffer;
+	bool isError = false;
+
+	int length = line.size();
+	for (int i = 0; i < length; i++)
+	{
+		char symbol = line[i];
+
+		bool isOpeningBracket = (symbol == '(') || (symbol == '[');
+		if (isOpeningBracket)
+		{
+			buffer.push(symbol);
+			continue;
+		}
+
+		bool isClosingBracket = (symbol == ')') || (symbol == ']');
+		if (isClosingBracket)
+		{
+			if (buffer.empty())
+			{
+				isError = true;
+				break;
+			}
+
+			char bufSymbol = buffer.top();
+			buffer.pop();
+
+			if (symbol == ')')
+			{
+				isError = (bufSymbol != '(');
+			}
+			else if (symbol == ']')
+			{
+				isError = (bufSymbol != '[');
+			}
+
+			if (isError)
+			{
+				break;
+			}
+		}
+	}
+
+	if (
+			isError
+			|| !buffer.empty()
+		)
+	{
+		return false;
+	}
+
+	return true;
 }
---
+++ 673 (parentheses check)/readme.txt
@@ -0,0 +1,2 @@
+673 - Parentheses Balance
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=103&page=show_problem&problem=614
