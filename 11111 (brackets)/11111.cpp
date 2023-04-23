b1cbbac6c60d853690c5908baa5b93b4b95bb9cd
hbaglaeva <helenbaglaeva@gmail.com>
2023-04-23 22:00:54 +0700

    #11111 in progress

--- 11111 (brackets)/11111.cpp
+++ 11111 (brackets)/11111.cpp
@@ -3,9 +3,29 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <stack>
 
 #define string std::string
 #define vector std::vector
+#define stack std::stack
+
+enum t_elementType
+{
+	e_INVALID = -1,
+	e_LEFT_BRACKET,
+	e_RIGHT_BRACKET,
+	e_TOY
+};
+
+struct StackElement
+{
+	StackElement () : m_type(e_INVALID), m_size(0) {}
+	StackElement (const t_elementType & type, const int & size) : m_type(type), m_size(size) {}
+
+	t_elementType m_type;
+	int m_size;
+};
+
 
 void processInput ();
 void processTestCase (const string & inputLine);
@@ -66,6 +86,64 @@ bool isCorrectMatrioshka (const vector<int> & inputNumbers)
 		return true;
 	}
 
-	//TODO...
-	return false;
+	stack<StackElement> buffer;
+	bool isError = false;
+
+	int numbersCount = inputNumbers.size();
+	for (int i = 0; i < numbersCount; i++)
+	{
+		int currentNumber = inputNumbers[i];
+		if (currentNumber < 0)
+		{
+			StackElement bufElement (e_LEFT_BRACKET, -currentNumber);
+			buffer.push(bufElement);
+		}
+		else if (currentNumber > 0)
+		{
+			int currentToySize = currentNumber;
+			int innerSum = 0;
+			while (
+					!buffer.empty()
+					&& buffer.top().m_type == e_TOY
+				)
+			{
+				innerSum += buffer.top().m_size;
+				buffer.pop();
+			}
+
+			if (
+					currentToySize <= innerSum
+					|| buffer.empty()
+					|| buffer.top().m_type != e_LEFT_BRACKET
+					|| buffer.top().m_size != currentToySize
+				)
+			{
+				isError = true;
+				break;
+			}
+
+			buffer.pop();
+			StackElement bufElement (e_TOY, currentNumber);
+			buffer.push(bufElement);
+		}
+	}
+
+	if (buffer.empty())
+	{
+		return true;
+	}
+
+	StackElement topElem = buffer.top();
+	if (topElem.m_type != e_TOY)
+	{
+		isError = true;
+	}
+
+	if (isError)
+	{
+		return false;
+	}
+
+	buffer.pop();
+	return buffer.empty();
 }
