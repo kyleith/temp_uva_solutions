5d45ef265aa871cdfc6416d3af9e485745f00267
hbaglaeva <helenbaglaeva@gmail.com>
2023-05-01 19:32:26 +0700

    #540 in progress

--- 540 (queue of queues)/540.cpp
+++ 540 (queue of queues)/540.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+#define unordered_map std::unordered_map
+#define string std::string
 
 void processInput ();
+void processTestCase (const int & teamsCount);
 
 int main ()
 {
@@ -16,5 +23,51 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int t = -1;
+	int scenarioIndex = 0;
+	while (
+			(scanf("%d\n", &t) == 1)
+			&& (t > 0)
+		)
+	{
+		++scenarioIndex;
+		printf("Scenario #%d\n", scenarioIndex);
+		processTestCase(t);
+		printf("\n");
+	}
+}
+
+void processTestCase (const int & teamsCount)
+{
+	unordered_map<int, int> personIdToTeamId;
+
+	for (int teamId = 0; teamId < teamsCount; teamId++)
+	{
+		int teamSize = -1;
+		scanf("%d", &teamSize);
+
+		for (int j = 0; j < teamSize; j++)
+		{
+			int personId = -1;
+			scanf("%d", &personId);
+			personIdToTeamId[personId] = teamId;
+		}
+	}
+
+	string currentCommand;
+	while (currentCommand != "STOP")
+	{
+		int personId = -1;
+		std::cin >> currentCommand;
+
+		if (currentCommand == "ENQUEUE")
+		{
+			std::cin >> personId;
+			//TODO: process ENQUEUE...
+		}
+		else if (currentCommand == "DEQUEUE")
+		{
+			//TODO: process ENQUEUE...
+		}
+	}
 }
