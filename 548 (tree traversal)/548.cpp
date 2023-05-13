3831afb2ce797357e2f4276c2e81e70a55f0ebd4
hbaglaeva <helenbaglaeva@gmail.com>
2023-05-13 23:24:15 +0700

    #548 in progress

--- 548 (tree traversal)/548.cpp
+++ 548 (tree traversal)/548.cpp
@@ -1,11 +1,15 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #define string std::string
 
+const int g_MAX_NODES_COUNT = 10000;
+
 void processInput ();
 void processTestCase (const string & inorderLine, const string & postorderLine);
+int parseElements (const string & line, int array []);
 
 int main ()
 {
@@ -33,8 +37,33 @@ void processInput ()
 
 void processTestCase (const string & inorderLine, const string & postorderLine)
 {
-	int leafValue = -1;
+	int inOrderElements[g_MAX_NODES_COUNT], postOrderElements[g_MAX_NODES_COUNT], indexInInorder[g_MAX_NODES_COUNT];
+	int n = 0;
+	n = parseElements(inorderLine, inOrderElements);
+	parseElements(postorderLine, postOrderElements);
+
+	for (int i = 0; i < n; i++)
+	{
+		int element = inOrderElements[i];
+		indexInInorder[element] = i;
+	}
+
+	int minSum = -1, leafValue = -1;
 	//TODO...
 
 	printf("%d\n", leafValue);
+}
+
+int parseElements (const string & line, int array [])
+{
+	std::istringstream ss(line);
+
+	int value, count = 0;
+	while (ss >> value)
+	{
+		array[count] = value;
+		count++;
+	}
+
+	return count;
 }
