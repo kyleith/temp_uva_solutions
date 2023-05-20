2748171f92c3f7607e0fc4ad6a26c5a6790b344e
hbaglaeva <helenbaglaeva@gmail.com>
2023-05-20 20:19:40 +0700

    #297 in progress

--- 297 (quadtrees)/297.cpp
+++ 297 (quadtrees)/297.cpp
@@ -4,6 +4,31 @@
 
 #define string std::string
 
+const int g_IMAGE_WIDTH = 32;
+const int g_IMAGE_HEIGHT = 32;
+const int g_COLOR_WHITE = 0;
+const int g_COLOR_BLACK = 255;
+
+class QuadTree
+{
+public:
+	QuadTree() {}
+	void decodeQuadTree (const string & inputLine);
+	bool isBlackPixel (const int & i, const int & j);
+private:
+	int m_image [g_IMAGE_WIDTH][g_IMAGE_HEIGHT];
+};
+
+void QuadTree::decodeQuadTree (const string & inputLine)
+{
+	//TODO...
+}
+
+bool QuadTree::isBlackPixel (const int & i, const int & j)
+{
+	return m_image[i][j] == g_COLOR_BLACK;
+}
+
 void processInput ();
 void processTestCase ();
 
@@ -33,9 +58,28 @@ void processInput ()
 void processTestCase ()
 {
 	string firstLine, secondLine;
+	QuadTree firstTree, secondTree;
+
 	std::getline(std::cin, firstLine);
 	std::getline(std::cin, secondLine);
 
-	int result = 0;//TODO...
-	printf("There are %d black pixels.\n", result);
+	firstTree.decodeQuadTree(firstLine);
+	secondTree.decodeQuadTree(secondLine);
+
+	int blackPixelsCount = 0;
+	for (int i = 0; i < g_IMAGE_WIDTH; i++)
+	{
+		for (int j = 0; j < g_IMAGE_HEIGHT; j++)
+		{
+			if (
+					firstTree.isBlackPixel(i, j)
+					|| secondTree.isBlackPixel(i, j)
+				)
+			{
+				blackPixelsCount++;
+			}
+		}
+	}
+
+	printf("There are %d black pixels.\n", blackPixelsCount);
 }
