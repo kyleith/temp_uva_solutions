fd613d3e758aae2f768ec5e6fc1c8e012f325436
hbaglaeva <helenbaglaeva@gmail.com>
2023-06-03 19:43:48 +0700

    #699 init

---
+++ 699 (tree traversal)/699.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+void processInput ();
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	processInput ();
+
+	return 0;
+}
+
+void processInput ()
+{
+	//TODO...
+}
