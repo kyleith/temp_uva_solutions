35db3fa0fed976eb2786fc82e443ee155060c157
hbaglaeva <helenbaglaeva@gmail.com>
2023-06-12 13:51:11 +0700

    #327 in progress

--- 327 (evaluating c expression)/327.cpp
+++ 327 (evaluating c expression)/327.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
+
+#define string std::string
 
 void processInput ();
+string getMinifiedExpression (const string & line);
+void evaluateExpression (const string & line);
 
 int main ()
 {
@@ -16,5 +22,27 @@ int main ()
 
 void processInput ()
 {
+	string inputLine;
+	while (std::getline(std::cin, inputLine))
+	{
+		string minifiedExpression = getMinifiedExpression(inputLine);
+		bool isValidExpression = (minifiedExpression.size() > 0);
+		if (isValidExpression)
+		{
+			evaluateExpression(inputLine);
+		}
+	}
+}
+
+string getMinifiedExpression (const string & line)
+{
+	string result = line;
+	//TODO...
+	return result;
+}
+
+void evaluateExpression (const string & line)
+{
+	printf("Expression: %s\n", line.c_str());
 	//TODO...
 }
