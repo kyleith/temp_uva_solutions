47ad88ff2974c9f01124bef23a31ac60d2701312
hbaglaeva <helenbaglaeva@gmail.com>
2023-06-12 15:09:10 +0700

    #327 in progress

--- 327 (evaluating c expression)/327.cpp
+++ 327 (evaluating c expression)/327.cpp
@@ -1,12 +1,27 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <queue>
 
 #define string std::string
+#define queue std::queue
+
+const int g_VARIABLES_COUNT = 26;
 
 void processInput ();
 string getMinifiedExpression (const string & line);
 void evaluateExpression (const string & minifiedExpression);
+
+void parseExpression (
+		const string & minifiedExpression,
+		queue<char> & preIncrement,
+		queue<char> & preDecrement,
+		queue<char> & postIncrement,
+		queue<char> & postDecrement,
+		string & mainExpression
+	);
+bool isLetter (const char & symbol);
+bool isOperator (const char & symbol);
 
 int main ()
 {
@@ -42,9 +57,7 @@ string getMinifiedExpression (const string & line)
 	for (int i = 0; i < lineLength; i++)
 	{
 		char symbol = line[i];
-		bool isLetter = ('a' <= symbol && symbol <= 'z');
-		bool isOperator = (symbol == '-' || symbol == '+');
-		if (isLetter || isOperator)
+		if (isLetter(symbol) || isOperator(symbol))
 		{
 			result.push_back(symbol);
 		}
@@ -54,5 +67,144 @@ string getMinifiedExpression (const string & line)
 
 void evaluateExpression (const string & minifiedExpression)
 {
-	//TODO...
+	int variableValues [g_VARIABLES_COUNT];
+	bool variableChanged [g_VARIABLES_COUNT];
+	int expressionValue = 0;
+
+	for (int i = 0; i < g_VARIABLES_COUNT; i++)
+	{
+		variableValues[i] = i + 1;
+		variableChanged[i] = false;
+	}
+
+	queue<char> preIncrement;
+	queue<char> preDecrement;
+	queue<char> postIncrement;
+	queue<char> postDecrement;
+	string mainExpression = "";
+
+	parseExpression(minifiedExpression, preIncrement, preDecrement, postIncrement, postDecrement, mainExpression);
+
+	while (!preIncrement.empty())
+	{
+		char operand = preIncrement.front();
+		preIncrement.pop();
+
+		int index = operand - 'a';
+		++variableValues[index];
+		variableChanged[index] = true;
+	}
+
+	while (!preDecrement.empty())
+	{
+		char operand = preDecrement.front();
+		preDecrement.pop();
+
+		int index = operand - 'a';
+		--variableValues[index];
+		variableChanged[index] = true;
+	}
+
+	//TODO: run main expression
+
+	while (!postIncrement.empty())
+	{
+		char operand = postIncrement.front();
+		postIncrement.pop();
+
+		int index = operand - 'a';
+		variableValues[index]++;
+		variableChanged[index] = true;
+	}
+
+	while (!postDecrement.empty())
+	{
+		char operand = postDecrement.front();
+		postDecrement.pop();
+
+		int index = operand - 'a';
+		variableValues[index]--;
+		variableChanged[index] = true;
+	}
+
+	printf("    value = %d\n", expressionValue);
+
+	for (int i = 0; i < g_VARIABLES_COUNT; i++)
+	{
+		if (variableChanged[i])
+		{
+			char variable = 'a' + i;
+			printf("    %c = %d\n", variable, variableValues[i]);
+		}
+	}
+}
+
+void parseExpression (
+		const string & minifiedExpression,
+		queue<char> & preIncrement,
+		queue<char> & preDecrement,
+		queue<char> & postIncrement,
+		queue<char> & postDecrement,
+		string & mainExpression
+	)
+{
+	mainExpression = "";
+
+	int length = minifiedExpression.size();
+	for (int i = 0; i < length; i++)
+	{
+		if (
+				i + 1 < length
+				&& isOperator(minifiedExpression[i])
+				&& isOperator(minifiedExpression[i + 1])
+				&& (minifiedExpression[i] == minifiedExpression[i + 1])
+			)
+		{
+			bool isIncrement = (minifiedExpression[i] == '+');
+			bool isDecrement = (minifiedExpression[i] == '-');
+			bool isPre = (i > 0 && isLetter(minifiedExpression[i - 1]));
+			bool isPost = (i + 2 < length && isLetter(minifiedExpression[i + 2]));
+
+			if (isPre)
+			{
+				char operand = minifiedExpression[i - 1];
+				if (isIncrement)
+				{
+					preIncrement.push(operand);
+				}
+				else if (isDecrement)
+				{
+					preDecrement.push(operand);
+				}
+			}
+			else if (isPost)
+			{
+				char operand = minifiedExpression[i + 2];
+				if (isIncrement)
+				{
+					postIncrement.push(operand);
+				}
+				else if (isDecrement)
+				{
+					postDecrement.push(operand);
+				}
+			}
+
+			i++;
+		}
+		else
+		{
+			mainExpression.push_back(minifiedExpression[i]);
+		}
+	}
+}
+
+bool isLetter (const char & symbol)
+{
+	return ('a' <= symbol && symbol <= 'z');
+}
+
+bool isOperator (const char & symbol)
+{
+	return (symbol == '-' || symbol == '+');
 }
