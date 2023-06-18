689a82143640f894a7309d863df48308dfc0a9ff
hbaglaeva <helenbaglaeva@gmail.com>
2023-06-18 20:17:25 +0700

    #839 in progress

--- 839 (tree traversal)/839.cpp
+++ 839 (tree traversal)/839.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define vector std::vector
+#define string std::string
 
 void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -16,5 +23,35 @@ int main ()
 
 void processInput ()
 {
+	int n;
+	scanf("%d\n\n", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	vector<string> treeLines;
+	string bufLine;
+	while (
+			std::getline(std::cin, bufLine)
+			&& bufLine.length() > 0
+		)
+	{
+		treeLines.push_back(bufLine);
+	}
+
+	bool isEquilibrium = true;
 	//TODO...
+	if (isEquilibrium)
+	{
+		printf("YES\n\n");
+	}
+	else
+	{
+		printf("NO\n\n");
+	}
 }
