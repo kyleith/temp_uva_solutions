590323944d8a2b04f58550f2a7ac72a2c4ab7566
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-02 15:53:51 +0700

    #572 in progress

--- 572 (connected components)/572.cpp
+++ 572 (connected components)/572.cpp
@@ -1,6 +1,44 @@
 #include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#define vector std::vector
+#define string std::string
 
 void processInput ();
+void processTestCase (const int & n, const int & m);
+
+class Graph
+{
+public:
+	Graph(): m_n(0), m_m(0) {}
+	void readGraph (const int & n, const int & m);
+	int calculateConnectedComponents ();
+private:
+	int m_n, m_m;
+	vector<string> m_area;
+};
+
+void Graph::readGraph (const int & n, const int & m)
+{
+	m_n = n;
+	m_m = m;
+
+	m_area.clear();
+
+	for (int i = 0; i < m_n; i++)
+	{
+		string buffer;
+		std::getline(std::cin, buffer);
+	}
+}
+
+int Graph::calculateConnectedComponents ()
+{
+	//TODO...
+	return 0;
+}
 
 int main ()
 {
@@ -16,5 +54,20 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n, m;
+	scanf("%d%d\n", &n, &m);
+
+	while (n > 0 && m > 0)
+	{
+		processTestCase(n, m);
+
+		scanf("%d%d\n", &n, &m);
+	}
+}
+
+void processTestCase (const int & n, const int & m)
+{
+	Graph currentArea;
+	currentArea.readGraph(n, m);
+	printf("%d\n", currentArea.calculateConnectedComponents());
 }
