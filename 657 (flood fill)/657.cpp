e81d28f10a6b7f26e8eeee3ff04f6904c84a9d91
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-03 14:18:21 +0700

    #657 in progress

--- 657 (flood fill)/657.cpp
+++ 657 (flood fill)/657.cpp
@@ -1,6 +1,44 @@
 #include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#define vector std::vector
+#define string std::string
 
 void processInput ();
+void processTestCase (const int & width, const int & height);
+
+class Picture
+{
+public:
+	Picture() : m_width(0), m_height(0) {}
+	void readPicture (const int & width, const int & height);
+	void decodeDots ();
+private:
+	vector<string> m_area;
+	int m_width, m_height;
+};
+
+void Picture::readPicture (const int & width, const int & height)
+{
+	m_width = width;
+	m_height = height;
+	m_area.clear();
+
+	for (int i = 0; i < m_height; i++)
+	{
+		string buffer;
+		std::getline(std::cin, buffer);
+
+		m_area.push_back(buffer);
+	}
+}
+
+void Picture::decodeDots ()
+{
+	//TODO...
+}
 
 int main ()
 {
@@ -16,5 +54,26 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int width = -1, height = -1;
+	int counter = 0;
+	scanf("%d%d\n", &width, &height);
+
+	while (
+		width > 0
+		&& height > 0
+	)
+	{
+		counter++;
+		printf("Throw %d\n", counter);
+
+		processTestCase(width, height);
+		scanf("%d%d\n", &width, &height);
+	}
+}
+
+void processTestCase (const int & width, const int & height)
+{
+	Picture currentPicture;
+	currentPicture.readPicture(width, height);
+	currentPicture.decodeDots();
 }
