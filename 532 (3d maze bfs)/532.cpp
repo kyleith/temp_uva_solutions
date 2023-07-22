15243da86f6e17216bac43f5160e183d2315bfa7
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-22 20:23:24 +0700

    #532 in progress

--- 532 (3d maze bfs)/532.cpp
+++ 532 (3d maze bfs)/532.cpp
@@ -1,6 +1,74 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
+
+const int g_MAX_MAZE_DIMENSION = 30;
+
+class Maze
+{
+public:
+	Maze() : m_levels(0), m_rows(0), m_columns(0) {}
+	void readMaze (const int & levels, const int & rows, const int & columns);
+	void tryToEscapeMaze ();
+private:
+	int m_levels, m_rows, m_columns;
+	char m_maze[g_MAX_MAZE_DIMENSION][g_MAX_MAZE_DIMENSION][g_MAX_MAZE_DIMENSION];
+	int m_distances[g_MAX_MAZE_DIMENSION][g_MAX_MAZE_DIMENSION][g_MAX_MAZE_DIMENSION];
+};
+
+void Maze::readMaze (const int & levels, const int & rows, const int & columns)
+{
+	m_levels = levels;
+	m_rows = rows;
+	m_columns = columns;
+
+	for (int i = 0; i < m_levels; i++)
+	{
+		for (int j = 0; j < m_rows; j++)
+		{
+			for (int k = 0; k < m_columns; k++)
+			{
+				m_maze[i][j][k] = '-';
+				m_distances[i][j][k] = -1;
+			}
+		}
+	}
+
+	string buffer;
+	for (int i = 0; i < m_levels; i++)
+	{
+		for (int j = 0; j < m_rows; j++)
+		{
+			std::getline(std::cin, buffer);
+			for (int k = 0; k < m_columns; k++)
+			{
+				m_maze[i][j][k] = buffer[k];
+			}
+		}
+		std::getline(std::cin, buffer);//read empty line
+	}
+}
+
+void Maze::tryToEscapeMaze ()
+{
+	bool foundExit = false;
+	int escapeTime = -1;
+	//TODO...
+
+	if (foundExit)
+	{
+		printf("Escaped in %d minute(s).\n", escapeTime);
+	}
+	else
+	{
+		printf("Trapped!\n");
+	}
+}
 
 void processInput ();
+void processMaze (const int & levels, const int & rows, const int & columns);
 
 int main ()
 {
@@ -16,5 +84,23 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int levels = -1, rows = -1, columns = -1;
+	scanf("%d%d%d\n", &levels, &rows, &columns);
+
+	while (
+		levels > 0
+		&& rows > 0
+		&& columns > 0
+	)
+	{
+		processMaze(levels, rows, columns);
+		scanf("%d%d%d\n", &levels, &rows, &columns);
+	}
+}
+
+void processMaze (const int & levels, const int & rows, const int & columns)
+{
+	Maze currentMaze;
+	currentMaze.readMaze(levels, rows, columns);
+	currentMaze.tryToEscapeMaze();
 }
