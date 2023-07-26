bf53e2035328176fd62dffcf562b49e8ba0f3290
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-26 23:45:25 +0700

    #10047 in progress

--- 10047 (state space search)/10047.cpp
+++ 10047 (state space search)/10047.cpp
@@ -7,11 +7,19 @@
 #define queue std::queue
 
 const int g_MAX_GRID_DIMENSION = 25;
+
 const int g_DIRECTIONS_COUNT = 4;
 const int g_DIRECTION_X[g_DIRECTIONS_COUNT] = {0, 1, 0, -1};
 const int g_DIRECTION_Y[g_DIRECTIONS_COUNT] = {-1, 0, 1, 0};
 
+const int g_COLORS_COUNT = 5;
 const int g_INFINITY = 2000;
+
+struct SearchState
+{
+	SearchState() : m_row(0), m_column(0), m_direction(0), m_color(0) {}
+	int m_row, m_column, m_direction, m_color;
+};
 
 class Graph
 {
@@ -151,7 +159,89 @@ int Graph::calculatePathTime ()
 {
 	int time = g_INFINITY;
 
-	//TODO...
+	int pathTimes[g_MAX_GRID_DIMENSION][g_MAX_GRID_DIMENSION][g_DIRECTIONS_COUNT][g_COLORS_COUNT];
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_columns; j++)
+		{
+			for (int k = 0; k < g_DIRECTIONS_COUNT; k++)
+			{
+				for (int l = 0; l < g_COLORS_COUNT; l++)
+				{
+					pathTimes[i][j][k][l] = g_INFINITY;
+				}
+			}
+		}
+	}
+
+	pathTimes[m_startRow][m_startColumn][0][0] = 0;
+
+	SearchState bufferState;
+	bufferState.m_row = m_startRow;
+	bufferState.m_column = m_startColumn;
+	bufferState.m_direction = 0;
+	bufferState.m_color = 0;
+
+	queue<SearchState> bufferQueue;
+	bufferQueue.push(bufferState);
+
+	while (!bufferQueue.empty())
+	{
+		bufferState = bufferQueue.front();
+		bufferQueue.pop();
+
+		int currentTime = pathTimes[bufferState.m_row][bufferState.m_column][bufferState.m_direction][bufferState.m_color];
+
+		int nextRow, nextColumn, nextDirection, nextColor;
+		SearchState nextState;
+
+		//try moving in current direction
+		nextColor = (bufferState.m_color + 1) % g_COLORS_COUNT;
+		nextDirection = bufferState.m_direction;
+		nextRow = bufferState.m_row + g_DIRECTION_Y[nextDirection];
+		nextColumn = bufferState.m_column + g_DIRECTION_X[nextDirection];
+		bool isValidRow = (0 <= nextRow && nextRow < m_rows);
+		bool isValidColumn = (0 <= nextColumn && nextColumn < m_columns);
+		if (
+			isValidRow
+			&& isValidColumn
+			&& pathTimes[nextRow][nextColumn][nextDirection][nextColor] >= g_INFINITY
+			&& m_maze[nextRow][nextColumn] != '#'
+		)
+		{
+			pathTimes[nextRow][nextColumn][nextDirection][nextColor] = currentTime + 1;
+			if (
+				m_maze[nextRow][nextColumn] == 'T'
+				&& nextColor == 0
+			)
+			{
+				time = pathTimes[nextRow][nextColumn][nextDirection][nextColor];
+				return time;
+			}
+			else
+			{
+				nextState.m_row = nextRow;
+				nextState.m_column = nextColumn;
+				nextState.m_direction = nextDirection;
+				nextState.m_color = nextColor;
+
+				bufferQueue.push(nextState);
+			}
+		}
+
+		//apply left and right rotation
+		nextState.m_row = bufferState.m_row;
+		nextState.m_column = bufferState.m_column;
+		nextState.m_color = bufferState.m_color;
+
+		nextState.m_direction = (bufferState.m_direction - 1 + g_DIRECTIONS_COUNT) % g_DIRECTIONS_COUNT;//ccw
+		pathTimes[nextState.m_row][nextState.m_column][nextState.m_direction][nextState.m_color] = currentTime + 1;
+		bufferQueue.push(nextState);
+
+		nextState.m_direction = (bufferState.m_direction + 1) % g_DIRECTIONS_COUNT;//cw
+		pathTimes[nextState.m_row][nextState.m_column][nextState.m_direction][nextState.m_color] = currentTime + 1;
+		bufferQueue.push(nextState);
+	}
 
 	return time;
 }
