626e02155c30d8323a9cd3625931ae37b440a299
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-27 17:48:07 +0700

    #10004 in progress

--- 10004 (bipartite graph)/10004.cpp
+++ 10004 (bipartite graph)/10004.cpp
@@ -1,6 +1,46 @@
 #include <cstdio>
 
+class Graph
+{
+public:
+	Graph () {}
+	void readGraph (const int & nodesCount);
+	void testBipartiteGraph ();
+private:
+};
+
+void Graph::readGraph (const int & nodesCount)
+{
+	int edgesCount;
+	scanf("%d\n", &edgesCount);
+
+	for (int i = 0; i < edgesCount; i++)
+	{
+		int u, v;
+		scanf("%d%d\n", &u, &v);
+
+		//TODO...
+	}
+}
+
+void Graph::testBipartiteGraph ()
+{
+	bool isBipartite = false;
+
+	//TODO...
+
+	if (isBipartite)
+	{
+		printf("BICOLORABLE.\n");
+	}
+	else
+	{
+		printf("NOT BICOLORABLE.\n");
+	}
+}
+
 void processInput ();
+void processTestCase (const int & nodesCount);
 
 int main ()
 {
@@ -16,5 +56,20 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n;
+	scanf("%d\n", &n);
+
+	while (n > 0)
+	{
+		processTestCase(n);
+
+		scanf("%d", &n);
+	}
+}
+
+void processTestCase (const int & nodesCount)
+{
+	Graph currentGraph;
+	currentGraph.readGraph(nodesCount);
+	currentGraph.testBipartiteGraph();
 }
