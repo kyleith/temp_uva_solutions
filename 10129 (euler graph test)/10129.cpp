4d5f0c3e86c1aa57f2cd18893473ed837ddcadf1
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-28 21:01:23 +0700

    #10129 in progress

--- 10129 (euler graph test)/10129.cpp
+++ 10129 (euler graph test)/10129.cpp
@@ -1,6 +1,50 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
+
+class Graph
+{
+public:
+	Graph() {}
+	void readGraph();
+	void testIfEulerGraph();
+private:
+};
+
+void Graph::readGraph()
+{
+	int edgesCount = -1;
+	scanf("%d\n", &edgesCount);
+
+	for (int i = 0; i < edgesCount; i++)
+	{
+		string word;
+		std::getline(std::cin, word);
+
+		//TODO...
+	}
+}
+
+void Graph::testIfEulerGraph()
+{
+	bool isEulerGraph = false;
+
+	//TODO...
+
+	if (isEulerGraph)
+	{
+		printf("Ordering is possible.\n");
+	}
+	else
+	{
+		printf("The door cannot be opened.\n");
+	}
+}
 
 void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -16,5 +60,18 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int testCases = -1;
+	scanf("%d", &testCases);
+
+	for (int i = 0; i < testCases; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	Graph currentGraph;
+	currentGraph.readGraph();
+	currentGraph.testIfEulerGraph();
 }
