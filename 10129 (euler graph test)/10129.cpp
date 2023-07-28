f0a90f9961401299aecb6ba6f1847ac8d09cc239
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-28 21:43:28 +0700

    #10129 in progress

--- 10129 (euler graph test)/10129.cpp
+++ 10129 (euler graph test)/10129.cpp
@@ -1,20 +1,51 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include <vector>
 
 #define string std::string
+#define vector std::vector
+
+const int g_GRAPH_NODES_COUNT = 'z' - 'a' + 1;
 
 class Graph
 {
 public:
-	Graph() {}
+	Graph();
 	void readGraph();
 	void testIfEulerGraph();
 private:
+	vector<bool> m_isActiveNode;
+	vector<vector<int>> m_adjacencyList;
+	int m_vertexDegree [g_GRAPH_NODES_COUNT];
+
+	void testVerticesDegrees (bool & isError, int & startNode);
+	void testGraphConnectivity (const int & startNode, bool & isError);
 };
+
+Graph::Graph()
+{
+	m_isActiveNode.reserve(g_GRAPH_NODES_COUNT);
+	m_adjacencyList.reserve(g_GRAPH_NODES_COUNT);
+
+	for (int i = 0; i < g_GRAPH_NODES_COUNT; i++)
+	{
+		vector<int> buffer;
+		m_adjacencyList.push_back(buffer);
+
+		m_isActiveNode.push_back(false);
+	}
+}
 
 void Graph::readGraph()
 {
+	for (int i = 0; i < g_GRAPH_NODES_COUNT; i++)
+	{
+		m_adjacencyList[i].clear();
+		m_isActiveNode[i] = false;
+		m_vertexDegree[i] = 0;
+	}
+
 	int edgesCount = -1;
 	scanf("%d\n", &edgesCount);
 
@@ -23,16 +54,31 @@ void Graph::readGraph()
 		string word;
 		std::getline(std::cin, word);
 
-		//TODO...
+		int u = word[0] - 'a';
+		int v = word[word.length() - 1] - 'a';
+
+		m_isActiveNode[u] = true;
+		m_isActiveNode[v] = true;
+
+		m_adjacencyList[u].push_back(v);
+
+		m_vertexDegree[u] -= 1;
+		m_vertexDegree[v] += 1;
 	}
 }
 
 void Graph::testIfEulerGraph()
 {
-	bool isEulerGraph = false;
+	bool isError = false;
+	int startNode = -1;
 
-	//TODO...
+	testVerticesDegrees(isError, startNode);
+	if (!isError)
+	{
+		testGraphConnectivity(startNode, isError);
+	}
 
+	bool isEulerGraph = !isError;
 	if (isEulerGraph)
 	{
 		printf("Ordering is possible.\n");
@@ -41,6 +87,74 @@ void Graph::testIfEulerGraph()
 	{
 		printf("The door cannot be opened.\n");
 	}
+}
+
+void Graph::testVerticesDegrees (bool & isError, int & startNode)
+{
+	int start = -1, end = -1;
+	int lastNode = -1;
+
+	for (int u = 0; u < g_GRAPH_NODES_COUNT; u++)
+	{
+		if (!m_isActiveNode[u])
+		{
+			continue;
+		}
+
+		lastNode = u;
+		int vertexDegree = m_vertexDegree[u];
+
+		bool isInvalidVertexDegree = (vertexDegree < -1 || 1 < vertexDegree);
+		if (isInvalidVertexDegree)
+		{
+			isError = true;
+			return;
+		}
+
+		if (vertexDegree == -1)
+		{
+			bool isStartOverrideAttempt = (start != -1);
+			if (isStartOverrideAttempt)
+			{
+				isError = true;
+				return;
+			}
+			start = u;
+		}
+		else if (vertexDegree == 1)
+		{
+			bool isEndOverrideAttempt = (end != -1);
+			if (isEndOverrideAttempt)
+			{
+				isError = true;
+				return;
+			}
+			end = u;
+		}
+	}
+
+	bool isStartNodeFound = (start != -1);
+	bool isEndNodeFound = (end != -1);
+
+	if (!isStartNodeFound && !isEndNodeFound)
+	{
+		isError = false;
+		startNode = lastNode;
+	}
+	else if (isStartNodeFound && isEndNodeFound)
+	{
+		isError = false;
+		startNode = start;
+	}
+	else
+	{
+		isError = true;
+	}
+}
+
+void Graph::testGraphConnectivity (const int & startNode, bool & isError)
+{
+	//TODO...
 }
 
 void processInput ();
