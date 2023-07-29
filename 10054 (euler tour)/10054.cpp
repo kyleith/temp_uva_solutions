3b64c3bd7410662d211a3ff0228cba22989c575a
hbaglaeva <helenbaglaeva@gmail.com>
2023-07-29 18:33:13 +0700

    #10054 in progress

--- 10054 (euler tour)/10054.cpp
+++ 10054 (euler tour)/10054.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+const int g_NODES_COUNT = 51;
+
 class Graph
 {
 public:
@@ -7,27 +9,86 @@ public:
 	void readGraph ();
 	void testEulerTour ();
 private:
+	int m_adjacencyMatrix[g_NODES_COUNT][g_NODES_COUNT];
+
+	void testVertexDegrees (bool & isError);
+	void testConnectivity (bool & isError);
+	void makeEulerTour ();
 };
 
 void Graph::readGraph ()
 {
-	//TODO...
+	for (int i = 0; i < g_NODES_COUNT; i++)
+	{
+		for (int j = 0; j < g_NODES_COUNT; j++)
+		{
+			m_adjacencyMatrix[i][j] = 0;
+		}
+	}
+
+	int edgesCount = -1;
+	scanf("%d", &edgesCount);
+
+	for (int k = 0; k < edgesCount; k++)
+	{
+		int u = -1, v = -1;
+		scanf("%d%d", &u, &v);
+
+		m_adjacencyMatrix[u][v]++;
+		m_adjacencyMatrix[v][u]++;
+	}
 }
 
 void Graph::testEulerTour ()
 {
-	bool isEulerTourPossible = false;
+	bool isError = false;
 
-	//TODO...
+	testVertexDegrees(isError);
+	if (!isError)
+	{
+		testConnectivity(isError);
+	}
 
+	bool isEulerTourPossible = !isError;
 	if (isEulerTourPossible)
 	{
-		//TODO...
+		makeEulerTour();
 	}
 	else
 	{
 		printf("some beads may be lost\n");
 	}
+}
+
+void Graph::testVertexDegrees (bool & isError)
+{
+	for (int u = 1; u < g_NODES_COUNT; u++)
+	{
+		int vertexDegree = 0;
+		for (int v = 1; v < g_NODES_COUNT; v++)
+		{
+			vertexDegree += m_adjacencyMatrix[u][v];
+		}
+
+		if (vertexDegree % 2 == 1)
+		{
+			isError = true;
+			return;
+		}
+	}
+
+	isError = false;
+}
+
+void Graph::testConnectivity (bool & isError)
+{
+	//isError = true;
+	//TODO...
+}
+
+void Graph::makeEulerTour ()
+{
+	//TODO...
 }
 
 void processInput ();
