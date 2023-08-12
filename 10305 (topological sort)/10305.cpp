8ad0316079dd5bfdd4254840e2a48d23f5dc44d9
hbaglaeva <helenbaglaeva@gmail.com>
2023-08-12 18:56:12 +0700

    #10305 in progress

--- 10305 (topological sort)/10305.cpp
+++ 10305 (topological sort)/10305.cpp
@@ -1,6 +1,34 @@
 #include <cstdio>
 
+class Graph
+{
+public:
+	Graph () {}
+	void readGraph (const int & nodesCount, const int & edgesCount);
+	void sortNodesTopologically ();
+private:
+};
+
+void Graph::readGraph (const int & nodesCount, const int & edgesCount)
+{
+	//TODO...
+
+	for (int i = 0; i < edgesCount; i++)
+	{
+		int u, v;
+		scanf("%d%d", &u, &v);
+	}
+}
+
+void Graph::sortNodesTopologically ()
+{
+	//TODO...
+
+	printf("\n");
+}
+
 void processInput ();
+void processTestCase (const int & n, const int & m);
 
 int main ()
 {
@@ -16,5 +44,19 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n, m;
+	while (
+		(scanf("%d%d", &n, &m) == 2)
+		&& (n > 0)
+	)
+	{
+		processTestCase(n, m);
+	}
+}
+
+void processTestCase (const int & n, const int & m)
+{
+	Graph currentGraph;
+	currentGraph.readGraph(n, m);
+	currentGraph.sortNodesTopologically();
 }
