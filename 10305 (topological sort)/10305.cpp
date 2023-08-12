abf42bec0dbfeabfc00e95f22c9144810951320a
hbaglaeva <helenbaglaeva@gmail.com>
2023-08-12 19:17:27 +0700

    #10305 solved AC

--- 10305 (topological sort)/10305.cpp
+++ 10305 (topological sort)/10305.cpp
@@ -1,4 +1,9 @@
 #include <cstdio>
+#include <vector>
+
+#define vector std::vector
+
+const int g_MAX_NODES_COUNT = 101;
 
 class Graph
 {
@@ -7,24 +12,77 @@ public:
 	void readGraph (const int & nodesCount, const int & edgesCount);
 	void sortNodesTopologically ();
 private:
+	vector<vector<int>> m_adjacencyList;
+	int m_nodesCount;
+	bool m_visited[g_MAX_NODES_COUNT];
+	vector<int> m_reversedTopoSort;
+
+	void dfs (int u);
 };
 
 void Graph::readGraph (const int & nodesCount, const int & edgesCount)
 {
-	//TODO...
+	m_nodesCount = nodesCount + 1;
+
+	m_adjacencyList.clear();
+	for (int i = 0; i < m_nodesCount; i++)
+	{
+		vector<int> buffer;
+		m_adjacencyList.push_back(buffer);
+	}
 
 	for (int i = 0; i < edgesCount; i++)
 	{
 		int u, v;
 		scanf("%d%d", &u, &v);
+
+		m_adjacencyList[u].push_back(v);
 	}
 }
 
 void Graph::sortNodesTopologically ()
 {
-	//TODO...
+	m_reversedTopoSort.clear();
+
+	for (int i = 1; i < m_nodesCount; i++)
+	{
+		m_visited[i] = false;
+	}
+	for (int i = 1; i < m_nodesCount; i++)
+	{
+		if (!m_visited[i])
+		{
+			dfs(i);
+		}
+	}
+
+	if (m_reversedTopoSort.size() > 0)
+	{
+		printf("%d", m_reversedTopoSort[m_reversedTopoSort.size() - 1]);
+	}
+
+	for (int i = m_reversedTopoSort.size() - 2; i >= 0; i--)
+	{
+		printf(" %d", m_reversedTopoSort[i]);
+	}
 
 	printf("\n");
+}
+
+void Graph::dfs (int u)
+{
+	m_visited[u] = true;
+
+	for (int i = 0; i < m_adjacencyList[u].size(); i++)
+	{
+		int v = m_adjacencyList[u][i];
+		if (!m_visited[v])
+		{
+			dfs(v);
+		}
+	}
+
+	m_reversedTopoSort.push_back(u);
 }
 
 void processInput ();
---
+++ 10305 (topological sort)/readme.txt
@@ -0,0 +1,2 @@
+10305 - Ordering Tasks
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1246
