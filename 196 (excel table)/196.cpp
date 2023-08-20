799adeefc0e5bf9326ac2d690e32ce9df7c5fa70
hbaglaeva <helenbaglaeva@gmail.com>
2023-08-20 19:43:39 +0700

    #196 in progress

--- 196 (excel table)/196.cpp
+++ 196 (excel table)/196.cpp
@@ -1,6 +1,50 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
+
+#define string std::string
 
 void processInput ();
+void processTestCase ();
+
+class Table
+{
+public:
+	Table () : m_rows(0), m_columns(0) {}
+	void readTable (const int & rows, const int & columns);
+	void processFormulas ();
+	void printTableValues ();
+private:
+	int m_rows, m_columns;
+};
+
+void Table::readTable (const int & rows, const int & columns)
+{
+	m_rows = rows;
+	m_columns = columns;
+
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_columns; j++)
+		{
+			string cellBuffer;
+			std::cin >> cellBuffer;
+
+			//TODO...
+		}
+	}
+}
+
+void Table::processFormulas ()
+{
+	//TODO...
+}
+
+void Table::printTableValues ()
+{
+	//TODO...
+	printf("\n");
+}
 
 int main ()
 {
@@ -16,5 +60,22 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int testCases = -1;
+	scanf("%d", &testCases);
+
+	for (int i = 0; i < testCases; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	int rows = -1, columns = -1;
+	scanf("%d%d", &columns, &rows);
+
+	Table currentTable;
+	currentTable.readTable(rows, columns);
+	currentTable.processFormulas();
+	currentTable.printTableValues();
 }
