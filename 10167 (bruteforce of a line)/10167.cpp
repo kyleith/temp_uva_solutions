ace7ba7bdba3e5bae72b3db100c9920631332e93
hbaglaeva <helenbaglaeva@gmail.com>
2023-08-27 18:36:15 +0700

    #10167 in progress

--- 10167 (bruteforce of a line)/10167.cpp
+++ 10167 (bruteforce of a line)/10167.cpp
@@ -1,6 +1,48 @@
 #include <cstdio>
+#include <vector>
+
+#define vector std::vector
+
+struct Point
+{
+	Point () : x(0), y(0) {}
+	int x, y;
+};
+
+class Plane
+{
+public:
+	Plane () : m_halfPoints(0), m_totalPoints(0) {}
+	void readPoints (const int & n);
+	void findBeelineCoeffs ();
+private:
+	int m_halfPoints, m_totalPoints;
+	vector<Point> m_points;
+};
+
+void Plane::readPoints (const int & n)
+{
+	m_halfPoints = n;
+	m_totalPoints = m_halfPoints * 2;
+
+	m_points.clear();
+	for (int i = 0; i < m_totalPoints; i++)
+	{
+		Point currentPoint;
+		scanf("%d%d", &currentPoint.x, &currentPoint.y);
+	}
+}
+
+void Plane::findBeelineCoeffs ()
+{
+	int a = -501, b = -501;
+	//TODO...
+
+	printf("%d %d\n", a, b);
+}
 
 void processInput ();
+void processTestCase (const int & n);
 
 int main ()
 {
@@ -16,5 +58,19 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n = -1;
+	while (
+		scanf("%d", &n) == 1
+		&& n > 0
+	)
+	{
+		processTestCase(n);
+	}
+}
+
+void processTestCase (const int & n)
+{
+	Plane currentPlane;
+	currentPlane.readPoints(n);
+	currentPlane.findBeelineCoeffs();
 }
