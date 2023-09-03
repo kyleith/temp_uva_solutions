45eb800e47e824957f6cf82eebb6a1a33be464d1
hbaglaeva <helenbaglaeva@gmail.com>
2023-09-03 19:14:05 +0700

    #11205 in progress

--- 11205 (bitmask bruteforce)/11205.cpp
+++ 11205 (bitmask bruteforce)/11205.cpp
@@ -1,6 +1,50 @@
 #include <cstdio>
 
+const int g_MAX_NUMBERS_COUNT = 100;
+
+class Pedometer
+{
+public:
+	Pedometer () : m_LEDsCount(0), m_numbersCount(0) {}
+	void readDisplayParams();
+	void calculateMinLEDsNumber();
+private:
+	int m_LEDsCount;
+	int m_numbersCount;
+	int m_numbers[g_MAX_NUMBERS_COUNT];
+};
+
+void Pedometer::readDisplayParams ()
+{
+	scanf("%d%d", &m_LEDsCount, &m_numbersCount);
+
+	for (int i = 0; i < m_numbersCount; i++)
+	{
+		int buffer = 0;
+		for (int j = 0; j < m_LEDsCount; j++)
+		{
+			int digit = 0;
+			scanf("%d", &digit);
+
+			buffer <<= 1;
+			buffer |= (digit & 1);
+		}
+
+		m_numbers[i] = buffer;
+	}
+}
+
+void Pedometer::calculateMinLEDsNumber ()
+{
+	int minLEDsCount = m_LEDsCount;
+
+	//TODO...
+
+	printf("%d\n", minLEDsCount);
+}
+
 void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -16,5 +60,18 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int testCasesCount = -1;
+	scanf("%d", &testCasesCount);
+
+	for (int i = 0; i < testCasesCount; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	Pedometer currentDisplay;
+	currentDisplay.readDisplayParams();
+	currentDisplay.calculateMinLEDsNumber();
 }
