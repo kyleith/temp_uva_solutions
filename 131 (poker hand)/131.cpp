8827ee60194f479686bc08c1988935958a676183
hbaglaeva <helenbaglaeva@gmail.com>
2023-09-09 17:17:42 +0700

    #131 in progress

--- 131 (poker hand)/131.cpp
+++ 131 (poker hand)/131.cpp
@@ -1,6 +1,37 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
+
+struct Card
+{
+	char face, suite;
+	int value;
+};
+
+class PokerRound
+{
+public:
+	PokerRound () {}
+	void readRoundCards (const string & inputLine);
+	void evaluateRound ();
+private:
+};
+
+void PokerRound::readRoundCards (const string & inputLine)
+{
+	//TODO...
+}
+
+void PokerRound::evaluateRound ()
+{
+	//TODO...
+	printf("\n");
+}
 
 void processInput ();
+void processTestCase (const string & inputLine);
 
 int main ()
 {
@@ -16,5 +47,16 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	string inputLine;
+	while (std::getline(std::cin, inputLine))
+	{
+		processTestCase(inputLine);
+	}
+}
+
+void processTestCase (const string & inputLine)
+{
+	PokerRound currentRound;
+	currentRound.readRoundCards(inputLine);
+	currentRound.evaluateRound();
 }
