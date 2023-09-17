a53ee2443280965bea8e08eb54e3ac6d1ebab215
hbaglaeva <helenbaglaeva@gmail.com>
2023-09-17 19:09:34 +0700

    #146 init

---
+++ 146 (next permutation)/146.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+void processInput ();
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	processInput();
+}
+
+void processInput ()
+{
+	//TODO...
+}
