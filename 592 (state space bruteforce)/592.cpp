d471c9265a458ca936197331bf68be40d8cd5558
hbaglaeva <helenbaglaeva@gmail.com>
2023-12-16 19:37:32 +0700

    #592 in progress

--- 592 (state space bruteforce)/592.cpp
+++ 592 (state space bruteforce)/592.cpp
@@ -1,4 +1,11 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
+
+void processInput ();
+void processTestCase (const int & statementsCount);
 
 int main ()
 {
@@ -7,12 +14,35 @@ int main ()
 	freopen("output.txt", "wt", stdout);
 #endif
 
-	void processInput();
+	processInput();
 
 	return 0;
 }
 
 void processInput ()
 {
-	//TODO...
+	int statementsCount = -1, counter = 0;
+	scanf("%d\n", &statementsCount);
+
+	while (statementsCount > 0)
+	{
+		++counter;
+		printf("Connversation #%d\n", counter);
+
+		processTestCase(statementsCount);
+		scanf("%d\n", &statementsCount);
+
+		printf("\n");
+	}
+}
+
+void processTestCase (const int & statementsCount)
+{
+	string line;
+
+	for (int i = 0; i < statementsCount; i++)
+	{
+		std::getline(std::cin, line);
+		//TODO...
+	}
 }
