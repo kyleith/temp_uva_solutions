153d34dec45251012129516b2b3a901066d08742
hbaglaeva <helenbaglaeva@gmail.com>
2023-12-16 19:13:35 +0700

    #592 init

---
+++ 592 (state space bruteforce)/592.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	void processInput();
+
+	return 0;
+}
+
+void processInput ()
+{
+	//TODO...
+}
