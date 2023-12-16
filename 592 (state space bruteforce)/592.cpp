a653de9f8a82990e7bf66a0ed7daf5c2096ae1fa
hbaglaeva <helenbaglaeva@gmail.com>
2023-12-16 20:51:18 +0700

    #592 in progress

--- 592 (state space bruteforce)/592.cpp
+++ 592 (state space bruteforce)/592.cpp
@@ -17,8 +17,10 @@ const string g_FLAG_NOT = "not";
 const string g_FLAG_LYING = "lying";
 
 const string g_ACTOR_IT = "It";
+const int g_ACTOR_IT_TYPES_COUNT = 2;
 const string g_DAY = "day";
 const string g_NIGHT = "night";
+const string g_ACTOR_IT_TYPES[g_ACTOR_IT_TYPES_COUNT] = {g_DAY, g_NIGHT};
 
 const string g_RESULT_IMPOSSIBLE = "This is impossible.";
 const string g_RESULT_NOTHING_PROOVED = "No facts are deducible.";
@@ -35,6 +37,7 @@ void readStatements (const int & statementsCount, vector<string> & statements);
 
 void testStatements (const vector<string> & statements, vector<string> & results);
 void bruteforceStates (const vector<string> & statements, vector<State> & validStates);
+bool isValidState (const vector<string> & statements, const State & inputState);
 void filterValidStates (const vector<State> & validStates, vector<string> & results);
 
 void printConversationResults (const vector<string> & results);
@@ -116,7 +119,43 @@ void bruteforceStates (const vector<string> & statements, vector<State> & validS
 {
 	validStates.clear();
 
+	State currentState;
+
+	for (int actorATypeIndex = 0; actorATypeIndex < g_ACTORS_TYPES_COUNT; actorATypeIndex++)
+	{
+		for (int actorBTypeIndex = 0; actorBTypeIndex < g_ACTORS_TYPES_COUNT; actorBTypeIndex++)
+		{
+			for (int actorCTypeIndex = 0; actorCTypeIndex < g_ACTORS_TYPES_COUNT; actorCTypeIndex++)
+			{
+				for (int actorDTypeIndex = 0; actorDTypeIndex < g_ACTORS_TYPES_COUNT; actorDTypeIndex++)
+				{
+					for (int actorETypeIndex = 0; actorETypeIndex < g_ACTORS_TYPES_COUNT; actorETypeIndex++)
+					{
+						for (int actorItTypeIndex = 0; actorItTypeIndex < g_ACTOR_IT_TYPES_COUNT; actorItTypeIndex++)
+						{
+							currentState.actorsTypes[0] = g_ACTORS_TYPES[actorATypeIndex];
+							currentState.actorsTypes[1] = g_ACTORS_TYPES[actorBTypeIndex];
+							currentState.actorsTypes[2] = g_ACTORS_TYPES[actorCTypeIndex];
+							currentState.actorsTypes[3] = g_ACTORS_TYPES[actorDTypeIndex];
+							currentState.actorsTypes[4] = g_ACTORS_TYPES[actorETypeIndex];
+							currentState.dayNight = g_ACTOR_IT_TYPES[actorItTypeIndex];
+
+							if (isValidState(statements, currentState))
+							{
+								validStates.push_back(currentState);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+bool isValidState (const vector<string> & statements, const State & inputState)
+{
 	//TODO...
+	return !false;
 }
 
 void filterValidStates (const vector<State> & validStates, vector<string> & results)
