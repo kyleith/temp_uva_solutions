1643dcfe9e13415f744c54c9f843a09434cc2a22
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-07 16:04:41 +0700

    #216 in progress

--- 216 (bruteforce)/216.cpp
+++ 216 (bruteforce)/216.cpp
@@ -1,7 +1,27 @@
 #include <cstdio>
+#include <cmath>
+#include <vector>
+#include <string>
+
+#define vector std::vector
+#define string std::string
 
 void processInput ();
 void processTestCase (const int & n);
+
+struct Point
+{
+	Point () : x(0.0), y(0.0) {}
+	double x, y;
+	double calculateCableDistance (const Point & targetPoint);
+};
+
+double Point::calculateCableDistance (const Point & targetPoint)
+{
+	double diffX = x - targetPoint.x;
+	double diffY = y - targetPoint.y;
+	return sqrt(diffX * diffX + diffY * diffY) + 16.0;
+}
 
 class Network
 {
@@ -11,10 +31,15 @@ public:
 	void findShortestCableChain ();
 private:
 	int m_nodesCount;
+	vector<Point> m_computers;
+
+	double calculateChainCableLength (const string & chain);
+	void printChainDescription (const string & chain);
 };
 
 void Network::readNodes (const int & n)
 {
+	m_computers.clear();
 	m_nodesCount = n;
 
 	for (int i = 0; i < m_nodesCount; i++)
@@ -22,7 +47,11 @@ void Network::readNodes (const int & n)
 		int x, y;
 		scanf("%d%d", &x, &y);
 
-		//TODO...
+		Point currentPoint;
+		currentPoint.x = x;
+		currentPoint.y = y;
+
+		m_computers.push_back(currentPoint);
 	}
 }
 
@@ -30,9 +59,49 @@ void Network::findShortestCableChain ()
 {
 	double totalLength = 0.0;
 
+	string currentChain = "";
+	for (int i = 0; i < m_nodesCount; i++)
+	{
+		char symbol = 'a' + i;
+		currentChain.push_back(symbol);
+	}
+
+	string bestChain = currentChain;
 	//TODO...
 
+	printChainDescription(bestChain);
+	totalLength = calculateChainCableLength(bestChain);
+
 	printf("Number of feet of cable required is %.2lf.\n", totalLength);
+}
+
+double Network::calculateChainCableLength (const string & chain)
+{
+	int itemsCount = chain.size();
+
+	double chainCableLength = 0.0;
+	for (int i = 0; i < itemsCount - 1; i++)
+	{
+		Point firstPoint = m_computers[chain[i] - 'a'];
+		Point secondPoint = m_computers[chain[i + 1] - 'a'];
+
+		chainCableLength += firstPoint.calculateCableDistance(secondPoint);
+	}
+
+	return chainCableLength;
+}
+
+void Network::printChainDescription (const string & chain)
+{
+	int itemsCount = chain.size();
+	for (int i = 0; i < itemsCount - 1; i++)
+	{
+		Point firstPoint = m_computers[chain[i] - 'a'];
+		Point secondPoint = m_computers[chain[i + 1] - 'a'];
+		double currentCableLength = firstPoint.calculateCableDistance(secondPoint);
+
+		printf("Cable requirement to connect (%d,%d) to (%d,%d) is %.2lf feet.\n", (int)firstPoint.x, (int)firstPoint.y, (int)secondPoint.x, (int)secondPoint.y, currentCableLength);
+	}
 }
 
 int main ()
