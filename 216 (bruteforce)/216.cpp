5c162cd1ac18afaaeeac721973c5760dd8019869
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-07 15:38:04 +0700

    #216 in progress

--- 216 (bruteforce)/216.cpp
+++ 216 (bruteforce)/216.cpp
@@ -1,6 +1,39 @@
 #include <cstdio>
 
 void processInput ();
+void processTestCase (const int & n);
+
+class Network
+{
+public:
+	Network () : m_nodesCount(0) {}
+	void readNodes (const int & n);
+	void findShortestCableChain ();
+private:
+	int m_nodesCount;
+};
+
+void Network::readNodes (const int & n)
+{
+	m_nodesCount = n;
+
+	for (int i = 0; i < m_nodesCount; i++)
+	{
+		int x, y;
+		scanf("%d%d", &x, &y);
+
+		//TODO...
+	}
+}
+
+void Network::findShortestCableChain ()
+{
+	double totalLength = 0.0;
+
+	//TODO...
+
+	printf("Number of feet of cable required is %.2lf.\n", totalLength);
+}
 
 int main ()
 {
@@ -15,5 +48,24 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n, testCaseIndex = 0;
+	scanf("%d", &n);
+
+	while (n > 0)
+	{
+		testCaseIndex++;
+
+		printf("**********************************************************\n");
+		printf("Network #%d\n", testCaseIndex);
+
+		processTestCase(n);
+		scanf("%d", &n);
+	}
+}
+
+void processTestCase (const int & n)
+{
+	Network currentNetwork;
+	currentNetwork.readNodes(n);
+	currentNetwork.findShortestCableChain();
 }
