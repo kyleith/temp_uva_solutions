d29fbfa93ad694893d07277d8741b755890469cd
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-14 18:13:11 +0700

    #693 in progress

--- 639 (bruteforce)/639.cpp
+++ 639 (bruteforce)/639.cpp
@@ -1,6 +1,43 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
 
 void processInput ();
+void processTestCase (const int & n);
+
+class Board
+{
+public:
+	Board() : m_n(0) {}
+	void readBoard(const int & n);
+	void findMaxRooksNumber();
+private:
+	int m_n;
+};
+
+void Board::readBoard(const int & n)
+{
+	m_n = n;
+
+	string buffer;
+	std::getline(std::cin, buffer);//reading previous line ending
+
+	for (int i = 0; i < m_n; i++)
+	{
+		std::getline(std::cin, buffer);
+		//TODO...
+	}
+}
+
+void Board::findMaxRooksNumber()
+{
+	int maxRooksNumber = 0;
+	//TODO...
+
+	printf("%d\n", maxRooksNumber);
+}
 
 int main ()
 {
@@ -15,5 +52,19 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n = -1;
+	scanf("%d", &n);
+
+	while (n > 0)
+	{
+		processTestCase(n);
+		scanf("%d", &n);
+	}
+}
+
+void processTestCase (const int & n)
+{
+	Board currentBoard;
+	currentBoard.readBoard(n);
+	currentBoard.findMaxRooksNumber();
 }
