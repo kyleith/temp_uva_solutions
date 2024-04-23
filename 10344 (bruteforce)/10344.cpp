0500dc1a242e55fec050453fb59f725bb492a2a3
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-23 21:07:34 +0700

    #10344 in progress

--- 10344 (bruteforce)/10344.cpp
+++ 10344 (bruteforce)/10344.cpp
@@ -1,4 +1,54 @@
 #include <cstdio>
+#include <vector>
+
+#define vector std::vector
+
+const int g_VARIABLES_COUNT = 5;
+
+class BruteforceSimulator
+{
+public:
+	BruteforceSimulator () {}
+	bool readValues ();
+	void testValues ();
+private:
+	vector<int> m_values;
+};
+
+bool BruteforceSimulator::readValues ()
+{
+	bool isSuccessfulRead = false;
+
+	m_values.clear();
+	for (int i = 0; i < g_VARIABLES_COUNT; i++)
+	{
+		int value;
+		scanf("%d", &value);
+		m_values.push_back(value);
+
+		if (value > 0)
+		{
+			isSuccessfulRead = true;
+		}
+	}
+
+	return isSuccessfulRead;
+}
+
+void BruteforceSimulator::testValues ()
+{
+	bool isPossible = false;
+	//TODO...
+
+	if (isPossible)
+	{
+		printf("Possible\n");
+	}
+	else
+	{
+		printf("Impossible\n");
+	}
+}
 
 void processInput ();
 
@@ -15,5 +65,9 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	BruteforceSimulator simulator;
+	while (simulator.readValues())
+	{
+		simulator.testValues();
+	}
 }
