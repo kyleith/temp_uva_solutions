3474dc67eeb72188ee814eb84fd843c2a439dc76
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-27 19:52:34 +0700

    #331 in progress

--- 331 (backtracking)/331.cpp
+++ 331 (backtracking)/331.cpp
@@ -1,6 +1,38 @@
 #include <cstdio>
 
+class BruteforceSimulator
+{
+public:
+	BruteforceSimulator () : m_n(0) {}
+	void readArray (const int & n);
+	void calculateSwapMaps (const int & index);
+private:
+	int m_n;
+};
+
+void BruteforceSimulator::readArray (const int & n)
+{
+	m_n = n;
+
+	for (int i = 0; i < m_n; i++)
+	{
+		int value;
+		scanf("%d", &value);
+
+		//TODO...
+	}
+}
+
+void BruteforceSimulator::calculateSwapMaps (const int & index)
+{
+	int count = 0;
+	//TODO...
+
+	printf("There are %d swap maps for input data set %d.\n", count, index);
+}
+
 void processInput ();
+void processTestCase (const int & n, const int & caseIndex);
 
 int main ()
 {
@@ -15,5 +47,20 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n, caseIndex = 0;
+	scanf("%d", &n);
+	while (n > 0)
+	{
+		caseIndex++;
+		processTestCase(n, caseIndex);
+
+		scanf("%d", &n);
+	}
+}
+
+void processTestCase (const int & n, const int & caseIndex)
+{
+	BruteforceSimulator simulator;
+	simulator.readArray(n);
+	simulator.calculateSwapMaps(caseIndex);
 }
