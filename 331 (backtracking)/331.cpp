6148e6c69093333dff184b74b414f8d6c90af230
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-27 20:12:46 +0700

    #331 solved AC

--- 331 (backtracking)/331.cpp
+++ 331 (backtracking)/331.cpp
@@ -1,5 +1,8 @@
 #include <cstdio>
 
+const int g_MAX_NUMBERS_COUNT = 5;
+const int g_INFINITY = 1000000;
+
 class BruteforceSimulator
 {
 public:
@@ -8,6 +11,11 @@ public:
 	void calculateSwapMaps (const int & index);
 private:
 	int m_n;
+	int m_numbers[g_MAX_NUMBERS_COUNT];
+	int m_savedSwapsCount, m_savedMapsCount;
+
+	void runBacktracking (int swapsCount);
+	void swapNumbers (const int & i, const int & j);
 };
 
 void BruteforceSimulator::readArray (const int & n)
@@ -19,16 +27,73 @@ void BruteforceSimulator::readArray (const int & n)
 		int value;
 		scanf("%d", &value);
 
-		//TODO...
+		m_numbers[i] = value;
 	}
 }
 
 void BruteforceSimulator::calculateSwapMaps (const int & index)
 {
-	int count = 0;
-	//TODO...
+	m_savedSwapsCount = g_INFINITY;
+	m_savedMapsCount = 0;
 
-	printf("There are %d swap maps for input data set %d.\n", count, index);
+	runBacktracking(0);
+
+	if (m_savedSwapsCount == 0)
+	{
+		m_savedMapsCount = 0;
+	}
+
+	printf("There are %d swap maps for input data set %d.\n", m_savedMapsCount, index);
+}
+
+void BruteforceSimulator::runBacktracking (int swapsCount)
+{
+	if (m_savedSwapsCount < swapsCount)
+	{
+		return;
+	}
+
+	bool isSorted = true;
+
+	for (int i = 0; i < m_n - 1; i++)
+	{
+		if (m_numbers[i] > m_numbers[i + 1])
+		{
+			isSorted = false;
+			break;
+		}
+	}
+
+	if (isSorted)
+	{
+		if (swapsCount < m_savedSwapsCount)
+		{
+			m_savedSwapsCount = swapsCount;
+			m_savedMapsCount = 1;
+		}
+		else if (swapsCount == m_savedSwapsCount)
+		{
+			m_savedMapsCount++;
+		}
+		return;
+	}
+
+	for (int i = 0; i < m_n - 1; i++)
+	{
+		if (m_numbers[i] > m_numbers[i + 1])
+		{
+			swapNumbers(i, i + 1);
+			runBacktracking(swapsCount + 1);
+			swapNumbers(i, i + 1);
+		}
+	}
+}
+
+void BruteforceSimulator::swapNumbers (const int & i, const int & j)
+{
+	int buffer = m_numbers[i];
+	m_numbers[i] = m_numbers[j];
+	m_numbers[j] = buffer;
 }
 
 void processInput ();
