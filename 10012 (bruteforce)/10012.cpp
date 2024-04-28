fa2928276a3cc501337a4c639b1ca9ea565a9ad4
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-28 21:14:59 +0700

    #10012 solved AC

--- 10012 (bruteforce)/10012.cpp
+++ 10012 (bruteforce)/10012.cpp
@@ -5,6 +5,8 @@
 
 #define vector std::vector
 
+const int g_MAX_CIRCLES_COUNT = 8;
+
 class BruteforceSimulator
 {
 public:
@@ -13,7 +15,8 @@ public:
 	void findMinRectangleSide ();
 private:
 	int m_circlesCount;
-	vector<double> m_circles;
+	vector<double> m_circlesRadiuses;
+	double m_circlesX[g_MAX_CIRCLES_COUNT];
 
 	double calculateCurrentRectangleSide ();
 	double calculateCircleCentersDistance (const double & r1, const double & r2);
@@ -21,7 +24,7 @@ private:
 
 void BruteforceSimulator::readCircles (const int & m)
 {
-	m_circles.clear();
+	m_circlesRadiuses.clear();
 
 	m_circlesCount = m;
 
@@ -29,7 +32,8 @@ void BruteforceSimulator::readCircles (const int & m)
 	{
 		double r;
 		scanf("%lf", &r);
-		m_circles.push_back(r);
+		m_circlesRadiuses.push_back(r);
+		m_circlesX[i] = 0.0;
 	}
 }
 
@@ -41,29 +45,59 @@ void BruteforceSimulator::findMinRectangleSide ()
 		return;
 	}
 
-	std::sort(m_circles.begin(), m_circles.end());
+	std::sort(m_circlesRadiuses.begin(), m_circlesRadiuses.end());
 
-	double minSide = calculateCurrentRectangleSide();
-	//TODO...
+	double minRectSide = calculateCurrentRectangleSide();
+	bool hasNextPermutation = std::next_permutation(m_circlesRadiuses.begin(), m_circlesRadiuses.end());
 
-	printf("%.3f\n", minSide);
+	while (hasNextPermutation)
+	{
+		double currentRectSide = calculateCurrentRectangleSide();
+		if(currentRectSide < minRectSide)
+		{
+			minRectSide = currentRectSide;
+		}
+
+		hasNextPermutation = std::next_permutation(m_circlesRadiuses.begin(), m_circlesRadiuses.end());
+	}
+
+	printf("%.3f\n", minRectSide);
 }
 
 double BruteforceSimulator::calculateCurrentRectangleSide ()
 {
-	double result = 0.0;
-	result += m_circles[0];
-	result += m_circles[m_circlesCount - 1];
+	m_circlesX[0] = m_circlesRadiuses[0];
 
 	for (int i = 0; i < m_circlesCount - 1; i++)
 	{
-		double r1 = m_circles[i];
-		double r2 = m_circles[i + 1];
-		double centersDistance = calculateCircleCentersDistance(r1, r2);
-		result += std::max(r1, std::max(r2, centersDistance));
+		m_circlesX[i + 1] = m_circlesX[i] + calculateCircleCentersDistance(m_circlesRadiuses[i], m_circlesRadiuses[i + 1]);
+
+		for (int j = 0; j < i; j++)
+		{
+			m_circlesX[i + 1] = std::max(m_circlesX[i + 1], m_circlesX[j] + calculateCircleCentersDistance(m_circlesRadiuses[j], m_circlesRadiuses[i + 1]));
+		}
 	}
 
-	return result;
+	double left = 0.0;
+	double right = m_circlesX[m_circlesCount - 1] + m_circlesRadiuses[m_circlesCount - 1];
+
+	for (int i = 0; i < m_circlesCount; i++)
+	{
+		double currentLeft = m_circlesX[i] - m_circlesRadiuses[i];
+		double currentRight = m_circlesX[i] + m_circlesRadiuses[i];
+
+		if (currentLeft < left)
+		{
+			left = currentLeft;
+		}
+
+		if (right < currentRight)
+		{
+			right = currentRight;
+		}
+	}
+
+	return right - left;
 }
 
 double BruteforceSimulator::calculateCircleCentersDistance (const double & r1, const double & r2)
