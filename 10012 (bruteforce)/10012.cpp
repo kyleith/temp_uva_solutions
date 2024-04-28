436427613f9c65760b08e408993b8f3c76fe85cc
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-04-28 20:20:31 +0700

    #10012 in progress

--- 10012 (bruteforce)/10012.cpp
+++ 10012 (bruteforce)/10012.cpp
@@ -1,4 +1,41 @@
 #include <cstdio>
+#include <vector>
+#include <algorithm>
+
+#define vector std::vector
+
+class BruteforceSimulator
+{
+public:
+	BruteforceSimulator () : m_circlesCount(0) {}
+	void readCircles (const int & m);
+	void findMinRectangleSide ();
+private:
+	int m_circlesCount;
+	vector<int> m_circles;
+};
+
+void BruteforceSimulator::readCircles (const int & m)
+{
+	m_circles.clear();
+
+	m_circlesCount = m;
+
+	for (int i = 0; i < m_circlesCount; i++)
+	{
+		int r;
+		scanf("%d", &r);
+		m_circles.push_back(r);
+	}
+}
+
+void BruteforceSimulator::findMinRectangleSide ()
+{
+	double minSide = 0;
+	//TODO...
+
+	printf("%.3f\n", minSide);
+}
 
 void processInput ();
 
@@ -15,5 +52,16 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int n;
+	scanf("%d", &n);
+
+	BruteforceSimulator simulator;
+	for (int i = 0; i < n; i++)
+	{
+		int m;
+		scanf("%d", &m);
+
+		simulator.readCircles(m);
+		simulator.findMinRectangleSide();
+	}
 }
