c5b112cb20aad2d1e6f846ec44b70ac8183f57a0
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2024-05-26 19:38:41 +0700

    #167 in progress

--- 167 (8 queens chess)/167.cpp
+++ 167 (8 queens chess)/167.cpp
@@ -1,6 +1,41 @@
 #include <cstdio>
 
+const int g_BOARD_SIDE_SIZE = 8;
+
+class Board
+{
+public:
+	Board () {}
+	void readBoardWeights ();
+	void findBestSolutionScore ();
+private:
+	int m_weights[g_BOARD_SIDE_SIZE][g_BOARD_SIDE_SIZE];
+};
+
+void Board::readBoardWeights ()
+{
+	for (int i = 0; i < g_BOARD_SIDE_SIZE; i++)
+	{
+		for (int j = 0; j < g_BOARD_SIDE_SIZE; j++)
+		{
+			int value;
+			scanf("%d", &value);
+
+			m_weights[i][j] = value;
+		}
+	}
+}
+
+void Board::findBestSolutionScore ()
+{
+	int score = 0;
+	//TODO...
+
+	printf("%5d\n", score);
+}
+
 void processInput ();
+void processTestCase ();
 
 int main ()
 {
@@ -15,5 +50,18 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	int k;
+	scanf("%d", &k);
+
+	for (int i = 0; i < k; i++)
+	{
+		processTestCase();
+	}
+}
+
+void processTestCase ()
+{
+	Board currentBoard;
+	currentBoard.readBoardWeights();
+	currentBoard.findBestSolutionScore();
 }
