b4827e84350ab9f636a5ac47475454a40909c218
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-03-16 15:34:53 +0700

    #140 in progress

--- 140 (strings bruteforce)/140.cpp
+++ 140 (strings bruteforce)/140.cpp
@@ -1,6 +1,30 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+#define string std::string
+
+class StringsGraph
+{
+public:
+	StringsGraph () {}
+	void readGraph (const string & line);
+	void findGraphWithMinBandwidth ();
+private:
+};
+
+void StringsGraph::readGraph (const string & line)
+{
+	//TODO...
+}
+
+void StringsGraph::findGraphWithMinBandwidth ()
+{
+	//TODO...
+}
 
 void processInput ();
+void processTestCase (const string & line);
 
 int main ()
 {
@@ -15,5 +39,22 @@ int main ()
 
 void processInput ()
 {
-	//TODO...
+	string inputLine;
+
+	while (std::getline(std::cin, inputLine))
+	{
+		if (inputLine[0] == '#')
+		{
+			break;
+		}
+
+		processTestCase(inputLine);
+	}
+}
+
+void processTestCase (const string & line)
+{
+	StringsGraph graph;
+	graph.readGraph(line);
+	graph.findGraphWithMinBandwidth();
 }
