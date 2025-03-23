88f40308e8a42664f4846c1f012316f975846a21
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-03-23 12:24:42 +0700

    #193 init

---
+++ 193 (bicolored graph)/193.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+void processTestCase ();
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	int testCasesCount = -1;
+	scanf("%d", &testCasesCount);
+
+	for (int i = 0; i < testCasesCount; i++)
+	{
+		processTestCase();
+	}
+
+	return 0;
+}
+
+void processTestCase ()
+{
+	//TODO...
+}
