0ba298ddec4ced0eb9c3939556d664d6cf0ad35c
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-04-06 15:06:37 +0700

    #10123 in progress

--- 10123 (backtracking of a balaced board)/10123.cpp
+++ 10123 (backtracking of a balaced board)/10123.cpp
@@ -10,18 +10,42 @@ const double g_RIGHT_FULCRUM_POSITION = 1.5;
 
 struct Package
 {
-	double position = 0.0, weight = 0.0;
-	bool isActive = false, isValidPosition = false;
+	Package () : position(0.0), weight(0.0), isActive(false), isValidPosition(false) {}
+	Package (const Package & copy);
+
+	double position, weight;
+	bool isActive, isValidPosition;
+
 	int getPosition () { return (int) position; }
 	int getWeight () { return (int) weight; }
 	double getPositionFromLeftFulcrum () { return position - g_LEFT_FULCRUM_POSITION; }
 	double getPositionFromRightFulcrum () { return position - g_RIGHT_FULCRUM_POSITION; }
+
+	Package & operator= (const Package & copy);
 };
+
+Package::Package (const Package & copy)
+{
+	position = copy.position;
+	weight = copy.weight;
+	isActive = copy.isActive;
+	isValidPosition = copy.isValidPosition;
+}
+
+Package & Package::operator= (const Package & copy)
+{
+	position = copy.position;
+	weight = copy.weight;
+	isActive = copy.isActive;
+	isValidPosition = copy.isValidPosition;
+
+	return *this;
+}
 
 class Board
 {
 public:
-	Board () { m_allPackages.reserve(g_MAX_PACKAGES_COUNT); }
+	Board () { m_allPackages.reserve(g_MAX_PACKAGES_COUNT); m_currentSolution.reserve(g_MAX_PACKAGES_COUNT); m_bestSolution.reserve(g_MAX_PACKAGES_COUNT); }
 	void readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount);
 	void findTippingSolution ();
 private:
@@ -29,8 +53,11 @@ private:
 	double m_F1LL, m_F1LR, m_F2LL, m_F2LR;
 	int m_packagesCount;
 	vector<Package> m_allPackages;
+	vector<Package> m_currentSolution, m_bestSolution;
+	bool m_solutionFound;
 
 	bool isBoardBalanced ();
+	void backtrackSolution (int n);
 };
 
 void Board::readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount)
@@ -71,7 +98,23 @@ void Board::findTippingSolution ()
 		return;
 	}
 
-	//TODO...
+	m_currentSolution.clear();
+	m_bestSolution.clear();
+	m_solutionFound = false;
+
+	backtrackSolution(0);
+
+	if (!m_solutionFound)
+	{
+		printf("Impossible\n");
+	}
+	else
+	{
+		for (int i = 0; i < m_bestSolution.size(); i++)
+		{
+			printf("%d %d\n", m_bestSolution[i].getPosition(), m_bestSolution[i].getWeight());
+		}
+	}
 }
 
 bool Board::isBoardBalanced ()
@@ -96,6 +139,42 @@ bool Board::isBoardBalanced ()
 	bool isF2Balanced = !(M2 > 0);
 
 	return isF1Balanced && isF2Balanced;
+}
+
+void Board::backtrackSolution (int n)
+{
+	if (n == m_packagesCount)
+	{
+		m_bestSolution.clear();
+		for (int i = 0; i < m_currentSolution.size(); i++)
+		{
+			m_bestSolution.push_back(m_currentSolution[i]);
+		}
+		m_solutionFound = true;
+		return;
+	}
+
+	for (int i = 0; i < m_packagesCount; i++)
+	{
+		if (m_solutionFound)
+		{
+			break;
+		}
+
+		if (m_allPackages[i].isActive)
+		{
+			m_allPackages[i].isActive = false;
+
+			if (isBoardBalanced())
+			{
+				m_currentSolution.push_back(m_allPackages[i]);
+				backtrackSolution(n + 1);
+				m_currentSolution.pop_back();
+			}
+
+			m_allPackages[i].isActive = true;
+		}
+	}
 }
 
 void processInput ();
