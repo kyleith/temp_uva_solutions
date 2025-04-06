c36866462b43f6b00f7732b0c599e6bebce4f4a7
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-04-06 13:37:29 +0700

    #10123 in progress

--- 10123 (backtracking of a balaced board)/10123.cpp
+++ 10123 (backtracking of a balaced board)/10123.cpp
@@ -1,14 +1,33 @@
 #include <cstdio>
+#include <vector>
+#include <cmath>
+
+#define vector std::vector
+
+const int g_MAX_PACKAGES_COUNT = 20;
+const int g_LEFT_FULCRUM_POSITION = -1.5;
+const int g_RIGHT_FULCRUM_POSITION = 1.5;
+
+struct Package
+{
+	double position = 0.0, weight = 0.0;
+	bool isActive = false, isValidPosition = false;
+	int getPosition () { return (int) position; }
+	int getWeight () { return (int) weight; }
+};
 
 class Board
 {
 public:
-	Board () {}
+	Board () { m_allPackages.reserve(g_MAX_PACKAGES_COUNT); }
 	void readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount);
 	void findTippingSolution ();
 private:
 	double m_boardLength, m_boardWeight;
 	int m_packagesCount;
+	vector<Package> m_allPackages;
+
+	bool isBoardBalanced ();
 };
 
 void Board::readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount)
@@ -17,16 +36,34 @@ void Board::readBoard (const int & boardLength, const int & boardWeight, const i
 	m_boardWeight = (double)boardWeight;
 	m_packagesCount = packagesCount;
 
+	m_allPackages.clear();
+
 	for (int i = 0; i < packagesCount; i++)
 	{
 		double position, weight;
-		scanf("%d%d", &position, &weight);
+		scanf("%lf%lf", &position, &weight);
+
+		bool isValidPosition = !(abs(position) > (m_boardLength / 2.0));
+
+		Package currentPackage;
+		currentPackage.position = position;
+		currentPackage.weight = weight;
+		currentPackage.isActive = true;
+		currentPackage.isValidPosition = isValidPosition;
+
+		m_allPackages.push_back(currentPackage);
 	}
 }
 
 void Board::findTippingSolution ()
 {
 	//TODO...
+}
+
+bool Board::isBoardBalanced ()
+{
+	//TODO...
+	return false;
 }
 
 void processInput ();
