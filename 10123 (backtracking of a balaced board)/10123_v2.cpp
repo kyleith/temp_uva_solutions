32c430f4b7086ac527623a7830d9a7840aaf6a3f
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-04-20 15:44:24 +0700

    #10123 v2 in progress

---
+++ 10123 (backtracking of a balaced board)/10123_v2.cpp
@@ -0,0 +1,253 @@
+#include <cstdio>
+#include <cmath>
+
+const int g_MAX_PACKAGES_COUNT = 20;
+const long double g_LEFT_FULCRUM_POSITION = -1.5;
+const long double g_RIGHT_FULCRUM_POSITION = 1.5;
+
+struct Package
+{
+	Package () : position(0.0), weight(0.0), isActive(false), isValidPosition(false), isCenterPosition(false) {}
+	Package (const Package & copy);
+
+	long double position, weight;
+	long double leverAbsForce;
+	long double LFF, RFF;
+	bool isActive, isValidPosition;
+	bool isCenterPosition;
+	int index;
+
+	int getPosition () { return (int) position; }
+	int getWeight () { return (int) weight; }
+	long double getPositionFromLeftFulcrum () { return position - g_LEFT_FULCRUM_POSITION; }
+	long double getPositionFromRightFulcrum () { return position - g_RIGHT_FULCRUM_POSITION; }
+
+	Package & operator= (const Package & copy);
+};
+
+bool comparePackages (const Package & A, const Package & B)
+{
+	return A.leverAbsForce < B.leverAbsForce;
+}
+
+Package::Package (const Package & copy)
+{
+	position = copy.position;
+	weight = copy.weight;
+	isActive = copy.isActive;
+	isValidPosition = copy.isValidPosition;
+	leverAbsForce = copy.leverAbsForce;
+	index = copy.index;
+	LFF = copy.LFF;
+	RFF = copy.RFF;
+	isCenterPosition = copy.isCenterPosition;
+}
+
+Package & Package::operator= (const Package & copy)
+{
+	position = copy.position;
+	weight = copy.weight;
+	isActive = copy.isActive;
+	isValidPosition = copy.isValidPosition;
+	leverAbsForce = copy.leverAbsForce;
+	index = copy.index;
+	LFF = copy.LFF;
+	RFF = copy.RFF;
+	isCenterPosition = copy.isCenterPosition;
+
+	return *this;
+}
+
+class Board
+{
+public:
+	Board () {}
+	void readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount);
+	void findTippingSolution ();
+private:
+	long double m_boardLength, m_boardWeight;
+	long double m_F1LL, m_F1LR, m_F2LL, m_F2LR;
+	int m_packagesCount, m_unbalancedPackagesCount;
+	Package m_allPackages [g_MAX_PACKAGES_COUNT];
+	int m_bestSolution [g_MAX_PACKAGES_COUNT];
+	bool m_solutionFound;
+
+	bool isBoardBalanced (const long double & totalLFF, const long double & totalRFF);
+	long double calculateTotalLFF ();
+	long double calculateTotalRFF ();
+};
+
+void Board::readBoard (const int & boardLength, const int & boardWeight, const int & packagesCount)
+{
+	m_boardLength = (long double)boardLength;
+	m_boardWeight = (long double)boardWeight;
+	m_packagesCount = packagesCount;
+	m_unbalancedPackagesCount = 0;
+
+	long double halfBoard = m_boardLength / 2.0;
+	m_F1LL = (halfBoard + g_LEFT_FULCRUM_POSITION) * (m_boardWeight / m_boardLength) * (-halfBoard - g_LEFT_FULCRUM_POSITION);
+	m_F1LR = (halfBoard - g_LEFT_FULCRUM_POSITION) * (m_boardWeight / m_boardLength) * (halfBoard - g_LEFT_FULCRUM_POSITION);
+	m_F2LL = (halfBoard + g_RIGHT_FULCRUM_POSITION) * (m_boardWeight / m_boardLength) * (-halfBoard - g_RIGHT_FULCRUM_POSITION);
+	m_F2LR = (halfBoard - g_RIGHT_FULCRUM_POSITION) * (m_boardWeight / m_boardLength) * (halfBoard - g_RIGHT_FULCRUM_POSITION);
+
+	for (int i = 0; i < packagesCount; i++)
+	{
+		long double position, weight;
+		scanf("%llf %llf", &position, &weight);
+
+		bool isValidPosition = !(fabsl(position) > (m_boardLength / 2.0));
+		bool isCenterPosition = (g_LEFT_FULCRUM_POSITION <= position && position <= g_RIGHT_FULCRUM_POSITION);
+
+		if (!isCenterPosition)
+		{
+			m_unbalancedPackagesCount++;
+		}
+
+		Package currentPackage;
+		currentPackage.position = position;
+		currentPackage.weight = weight;
+		currentPackage.isActive = true;
+		currentPackage.isValidPosition = isValidPosition;
+		currentPackage.isCenterPosition = isCenterPosition;
+
+		if (currentPackage.getPositionFromLeftFulcrum() < 0.0)
+		{
+			currentPackage.leverAbsForce = fabsl(currentPackage.getPositionFromLeftFulcrum()) * currentPackage.weight;
+		}
+		else if (currentPackage.getPositionFromRightFulcrum() > 0.0)
+		{
+			currentPackage.leverAbsForce = fabsl(currentPackage.getPositionFromRightFulcrum()) * currentPackage.weight;
+		}
+		else
+		{
+			currentPackage.leverAbsForce = 0.0;
+		}
+
+		currentPackage.LFF = currentPackage.getPositionFromLeftFulcrum() * currentPackage.weight;
+		currentPackage.RFF = currentPackage.getPositionFromRightFulcrum() * currentPackage.weight;
+
+		m_allPackages[i] = currentPackage;
+	}
+
+	for (int i = 0; i < packagesCount; i++)
+	{
+		m_allPackages[i].index = i;
+	}
+
+	int lastPackageIndex = 0;
+	for (int i = m_unbalancedPackagesCount; i < packagesCount; i++)
+	{
+		for (int j = lastPackageIndex; j < packagesCount; j++)
+		{
+			if (m_allPackages[j].isCenterPosition)
+			{
+				lastPackageIndex = j;
+				break;//for j
+			}
+		}
+		m_bestSolution[i] = lastPackageIndex;
+	}
+}
+
+void Board::findTippingSolution ()
+{
+	//TODO...
+
+	m_solutionFound = false;
+
+	if (m_solutionFound)
+	{
+		for (int i = 0; i < m_packagesCount; i++)
+		{
+			int index = m_bestSolution[i];
+			printf("%d %d\n", m_allPackages[index].getPosition(), m_allPackages[index].getWeight());
+		}
+	}
+	else
+	{
+		printf("Impossible\n");
+	}
+}
+
+bool Board::isBoardBalanced (const long double & totalLFF, const long double & totalRFF)
+{
+	bool isF1Balanced = !(totalLFF < 0);
+	bool isF2Balanced = !(totalRFF > 0);
+
+	return isF1Balanced && isF2Balanced;
+}
+
+long double Board::calculateTotalLFF ()
+{
+	long double M1 = m_F1LL + m_F1LR;
+
+	for (int i = 0; i < m_packagesCount; i++)
+	{
+		const Package & currentPackage = m_allPackages[i];
+		if (
+			!currentPackage.isValidPosition
+			|| !currentPackage.isActive
+		)
+		{
+			continue;
+		}
+
+		M1 += currentPackage.LFF;
+	}
+
+	return M1;
+}
+
+long double Board::calculateTotalRFF ()
+{
+	long double M2 = m_F2LL + m_F2LR;
+
+	for (int i = 0; i < m_packagesCount; i++)
+	{
+		const Package & currentPackage = m_allPackages[i];
+		if (
+			!currentPackage.isValidPosition
+			|| !currentPackage.isActive
+		)
+		{
+			continue;
+		}
+
+		M2 += currentPackage.RFF;
+	}
+
+	return M2;
+}
+
+void processInput ();
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	processInput();
+
+	return 0;
+}
+
+void processInput ()
+{
+	int length = -1, weight = -1, packagesCount = -1;
+	int caseIndex = 0;
+	Board currentBoard;
+
+	while (
+		(scanf("%d%d%d", &length, &weight, &packagesCount) == 3)
+		&& !(length == 0 && weight == 0 && packagesCount == 0)
+	)
+	{
+		caseIndex++;
+		printf("Case %d:\n", caseIndex);
+
+		currentBoard.readBoard(length, weight, packagesCount);
+		currentBoard.findTippingSolution();
+	}
+}
