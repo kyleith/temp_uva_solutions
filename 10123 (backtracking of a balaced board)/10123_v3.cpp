de43231e5f396f1c0d9c80bed004a724f36a26e9
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-04-27 19:02:33 +0700

    #10123 v3 init

---
+++ 10123 (backtracking of a balaced board)/10123_v3.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+void processInput();
+
+int main ()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	processInput();
+
+	return 0;
+}
+
+void processInput ()
+{
+	int length = -1, weight = -1, packagesCount = -1;
+	int caseIndex = 0;
+
+	while (
+		(scanf("%d%d%d", &length, &weight, &packagesCount) == 3)
+		&& !(length == 0 && weight == 0 && packagesCount == 0)
+	)
+	{
+		caseIndex++;
+		printf("Case %d:\n", caseIndex);
+
+		//currentBoard.readBoard(length, weight, packagesCount);
+		//currentBoard.findTippingSolution();
+	}
+}
