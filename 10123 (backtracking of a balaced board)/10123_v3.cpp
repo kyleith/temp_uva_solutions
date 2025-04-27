839dbfff16a69668af9b943bff033e53626c662a
Elena Baglaeva <1725881+kyleith@users.noreply.github.com>
2025-04-27 19:23:33 +0700

    #10123 v3 in progress

--- 10123 (backtracking of a balaced board)/10123_v3.cpp
+++ 10123 (backtracking of a balaced board)/10123_v3.cpp
@@ -1,6 +1,20 @@
 #include <cstdio>
 
+const int g_MAX_PACKAGES_COUNT = 20;
+const double g_LEFT_FULCRUM_POSITION = -1.5;
+const double g_RIGHT_FULCRUM_POSITION = 1.5;
+
+double g_boardLength, g_boardWeight;
+double g_F1LL, g_F1LR, g_F2LL, g_F2LR;
+int g_packagesCount, g_unbalancedPackagesCount;
+
+double g_positionsX [g_MAX_PACKAGES_COUNT], g_weights [g_MAX_PACKAGES_COUNT];
+double g_LFF [g_MAX_PACKAGES_COUNT], g_RFF [g_MAX_PACKAGES_COUNT];
+bool g_isActive [g_MAX_PACKAGES_COUNT], g_isCenterPosition [g_MAX_PACKAGES_COUNT];
+
 void processInput();
+void readBoard(const int & boardLength, const int & boardWeight, const int & packagesCount);
+void findTippingSolution();
 
 int main ()
 {
@@ -27,7 +41,47 @@ void processInput ()
 		caseIndex++;
 		printf("Case %d:\n", caseIndex);
 
-		//currentBoard.readBoard(length, weight, packagesCount);
-		//currentBoard.findTippingSolution();
+		readBoard(length, weight, packagesCount);
+		findTippingSolution();
 	}
+}
+
+void readBoard(const int & boardLength, const int & boardWeight, const int & packagesCount)
+{
+	g_boardLength = (double)boardLength;
+	g_boardWeight = (double)boardWeight;
+	g_packagesCount = packagesCount;
+	g_unbalancedPackagesCount = 0;
+
+	double halfBoard = g_boardLength / 2.0;
+	g_F1LL = (halfBoard + g_LEFT_FULCRUM_POSITION) * (g_boardWeight / g_boardLength) * (-halfBoard - g_LEFT_FULCRUM_POSITION);
+	g_F1LR = (halfBoard - g_LEFT_FULCRUM_POSITION) * (g_boardWeight / g_boardLength) * (halfBoard - g_LEFT_FULCRUM_POSITION);
+	g_F2LL = (halfBoard + g_RIGHT_FULCRUM_POSITION) * (g_boardWeight / g_boardLength) * (-halfBoard - g_RIGHT_FULCRUM_POSITION);
+	g_F2LR = (halfBoard - g_RIGHT_FULCRUM_POSITION) * (g_boardWeight / g_boardLength) * (halfBoard - g_RIGHT_FULCRUM_POSITION);
+
+	for (int i = 0; i < packagesCount; i++)
+	{
+		double position, weight;
+		scanf("%lf %lf", &position, &weight);
+
+		bool isCenterPosition = (g_LEFT_FULCRUM_POSITION <= position && position <= g_RIGHT_FULCRUM_POSITION);
+
+		if (!isCenterPosition)
+		{
+			g_unbalancedPackagesCount++;
+		}
+
+		g_positionsX[i] = position;
+		g_weights[i] = weight;
+		g_isActive[i] = true;
+		g_isCenterPosition[i] = isCenterPosition;
+
+		g_LFF[i] = (position - g_LEFT_FULCRUM_POSITION) * weight;
+		g_RFF[i] = (position - g_RIGHT_FULCRUM_POSITION) * weight;
+	}
+}
+
+void findTippingSolution()
+{
+	//TODO...
 }
